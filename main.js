6cf617da8781f6321248ff2f81af8ef9d8c98300
axai <02b3akshay@gmail.com>
2023-08-03 22:31:57 +0530

    Add Arrays-0

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 'use strict';
 
-var user = {
-  name: 'Michelle Roberts',
-  age: 28,
-  location: {
-    city: 'New York',
-    country: 'US',
-  },
-};
+var fruits = ['Apple', 'Kiwi', 'Banana', 'Strawberry', 'Mango'];
 
-user.isAdmin = false;
+fruits.push('Avacado');
+fruits.pop();
+fruits[4] = 'Avacado';
+fruits[5] = 'Pineapple';
+// fruits[10] = 'Blueberries';
+console.log(`I love ${fruits[0]}`);
+console.log(`I also like ${fruits[1]}`);
+console.log(fruits.length);
+console.log(`Let's have a ${fruits[fruits.length - 1]} milkshake!`);
 
-console.log('User: ', user.name);
-console.log('City: ', user.location.city);
-console.log('Age: ', user.age);
-console.log('Is User Administrator?', user.isAdmin);
+// console.log(fruits);
