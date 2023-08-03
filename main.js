f6e1f0790c9ec05631876b6c5ac3323b318c8805
axai <02b3akshay@gmail.com>
2023-08-03 22:35:30 +0530

    Add Arrays in JavaScript

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var fruits = ['Apple', 'Kiwi', 'Banana', 'Strawberry', 'Mango'];
+var bestsellers = [
+  'Contact',
+  'Deception Point',
+  'Digital Fortress',
+  'Jurassic Park',
+  'Shantaram',
+];
 
-fruits.push('Avacado');
-fruits.pop();
-fruits[4] = 'Avacado';
-fruits[5] = 'Pineapple';
-// fruits[10] = 'Blueberries';
-console.log(`I love ${fruits[0]}`);
-console.log(`I also like ${fruits[1]}`);
-console.log(fruits.length);
-console.log(`Let's have a ${fruits[fruits.length - 1]} milkshake!`);
+bestsellers.push('Freakonomics');
 
-// console.log(fruits);
+bestsellers[1] = 'The Lord of the Rings';
+
+console.log('Bestsellers: ', bestsellers.toString());
+
