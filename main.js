119dc1ba8920f1aef71c0e4b95e468194676ae4d
axai <02b3akshay@gmail.com>
2023-08-03 21:32:53 +0530

    Add Booleans-1

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ var forceDelete = false;
 var message = ''; //hey
 var showPostButton = !!message;
 console.log(`Show Post Button: ${showPostButton}`); //true
+
+var numOfCharacters = 0; //2
+var showEditButton = !!numOfCharacters;
+console.log(`Show Edit Button: ${showEditButton}`); //true
