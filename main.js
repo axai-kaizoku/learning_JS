42a79c8e6d88593c2f290c1145e9b312a4f77968
axai <02b3akshay@gmail.com>
2023-08-03 21:21:57 +0530

    Add Playing with Strings-0

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var septillion = 1_000_000_000_000_000_000_000_000n;
-var plusOne = septillion + 1n;
+'use strict';
 
-console.log('Septillion plus one is equal to', plusOne);
+
+var user = 'John Mockery';
+var designation = 'Sr. Designer';
+var origanization = 'Sentinel Design';
+var city = 'New York';
+var country = 'USA';
+var location = city + ', ' + country;
+var favQuote = 'To \'single-quote\' or "double-quote"';
+
+var employeeData = `Name: ${user}
+Designation: ${designation}
+Organization: ${origanization}
+Location: ${location}`;
+
+console.log(employeeData);
