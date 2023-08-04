708df71edc600a901fdd90f224b2bd8c4e37886f
axai <02b3akshay@gmail.com>
2023-08-04 14:20:54 +0530

    Add Understanding var-1

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ var user = 'Sarah Rice';
 
 function sayHi() {
   var greeting = "It's so good to meet you!";
+  function greet() {
+    return `She also says, ${greeting}`;
+  }
   console.log(`${user} says Hi!`);
-  console.log(greeting);
+  console.log(greet());
 }
 
 sayHi();
