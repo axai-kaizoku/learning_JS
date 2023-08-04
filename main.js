e762fc21193590b577316108248d8608cf24830c
axai <02b3akshay@gmail.com>
2023-08-04 14:17:44 +0530

    Add Understanding var-0

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-'use strict';
+var user = 'Sarah Rice';
 
-// Compute a sum
-var a = '10';
-var b = 20;
-try {
-  if (typeof a != typeof b)
-    throw new Error('Both operands must belong to the same data type!');
-  var c = a + b;
-  console.log(`The result of the sum is ${c}`);
-} catch (error) {
-  console.log(error);
-} finally {
-  console.log('I will run no matter what!');
+function sayHi() {
+  var greeting = "It's so good to meet you!";
+  console.log(`${user} says Hi!`);
+  console.log(greeting);
 }
 
-// Display the value of PI
-var pi = Math.PI;
-console.log(`The value of PI is ${pi}`);
+sayHi();
