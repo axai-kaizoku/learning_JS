92150cd63c6a7d07c4f8d7a072f41e1c7a66409d
axai <02b3akshay@gmail.com>
2023-08-04 23:02:31 +0530

    Add Lots of Choices with 'else if' - 0

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 
-let temperature = 25; // use other values
 
-if (temperature < 15) {
-  console.log("It's cold outside");
+let currentHour = new Date().getHours();
+// console.log(currentHour)
+
+if (currentHour >= 0 && currentHour < 12) {
+  console.log('Good Morning!');
+} else if (currentHour >= 12 && currentHour < 16) {
+  console.log('Good Afternoon!');
+} else if (currentHour >= 16 && currentHour < 20) {
+  console.log('Good Evening!');
 } else {
-  console.log("It's warm outside");
+  console.log('Good Night!');
 }
