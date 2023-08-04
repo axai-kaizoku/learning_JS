2e1952cd4712bb5867d1deec8d1f0b9b6385c317
axai <02b3akshay@gmail.com>
2023-08-04 23:09:48 +0530

    Add Switch-0

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 'use strict';
 
+let a = 20;
+let b = 50;
+// let action = 'ADD';
+// let action = 'SUBTRACT';
+let action = 'MULTIPLY';
+// let action = 'DIVIDE';
 
-'use strict';
-
-let grade;
-let percentage = 71;
-
-
-
-if (percentage <= 100 && percentage >= 91) {
-    grade = 'A+';
-} else if (percentage <= 90 && percentage >= 81) {
-    grade = 'B+';
-} else if (percentage <= 80 && percentage >= 71) {
-    grade = 'C+';
-} else if (percentage <= 70 && percentage >= 61) {
-    grade = 'D+';
-} else {
-    grade = 'Fail';
+switch (action) {
+  case 'ADD': {
+    let result = a + b;
+    console.log(`The sum of ${a} and ${b} is ${result}`);
+    break;
+  }
+  case 'SUBTRACT': {
+    let result = a - b;
+    console.log(`The subtraction of ${a} and ${b} is ${result}`);
+    break;
+  }
+  case 'MULTIPLY': {
+    let result = a * b;
+    console.log(`The multiplication of ${a} and ${b} is ${result}`);
+    break;
+  }
+  case 'DIVIDE': {
+    let result = a / b;
+    console.log(`The division of ${a} and ${b} is ${result}`);
+    break;
+  }
 }
-
-console.log(`The student has secured ${grade}`);
