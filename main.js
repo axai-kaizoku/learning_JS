985ef6b476f1a9fa7548735132f669fe4149fd99
axai <02b3akshay@gmail.com>
2023-08-05 22:25:50 +0530

    Add The 'this' Keyword & Function Execution Context-1

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 'use script';
 
-const person = {
-  name: 'John Mockery',
-  age: 19,
-  profession: 'Software Engineer',
-  sayHi() {
-    return `Hi, my name is ${this.name}. I'm ${this.age} years old and I'm a ${this.profession}.`;
-  },
-  canDrive() {
-    return `Can he drive ? \n${this.age >= 18 ? 'Yes' : 'No'}`;
-  },
+const john = {
+  age: 35,
+  gender: 'M',
+  weight: 78,
+  height: 177.8,
 };
 
-person.myJob = function () {
-  return `I work as a ${this.profession}`;
+const sarah = {
+  age: 32,
+  gender: 'F',
+  weight: 65,
+  height: 177.8,
 };
 
-console.log(person.sayHi());
-console.log(person.myJob());
-console.log(person.canDrive());
+const calcBmi = function () {
+  return this.weight / (this.height / 100) ** 2;
+};
+
+const calcBmr = function () {
+  if (this.gender === 'M') {
+    return 66.47 + 13.75 * this.weight + 5.003 * this.height - 6.755 * this.age;
+  } else {
+    return 655.1 + 9.563 * this.weight + 1.85 * this.height - 4.676 * this.age;
+  }
+};
+
+console.log(`John's BMI: ${calcBmi.call(john)}`);
+console.log(`John's BMR: ${calcBmr.call(john)}`);
+console.log(`sarah's BMI: ${calcBmi.call(sarah)}`);
+console.log(`sarah's BMR: ${calcBmr.call(sarah)}`);
