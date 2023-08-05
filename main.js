240d617377f7f6abb7e8cf3d2ce59dd58ba9e5c0
axai <02b3akshay@gmail.com>
2023-08-05 07:48:35 +0530

    Add Binary Logical Operators-0

--- main.js
+++ main.js
@@ -1,40 +1,12 @@
 'use strict';
 
-let today = new Date().getDay(); // 11; 
+// Logical OR
+let user = '';
+let greeting = `Hello, ${user || 'Stranger'}`;
+// console.log(greeting)
 
-// console.log(today);
 
-let day = 
-  today === 0
-  ? 'Sunday'
-  : today === 1
-  ? 'Monday'
-  : today === 2
-  ? 'Tuesday'
-  : today === 3
-  ? 'Wednesday'
-  : today === 4
-  ? 'Thursday'
-  : today === 5
-  ? 'Friday'
-  : today === 6
-  ? 'Saturday'
-  : 'not exactly a real day...';
 
-// console.log(`Today is ${day}`)
-
-// Actually this is the main part of the excercise
-
-// We have to manually give the input for 'DAY'.
-// But now it's not required.
-
-let DAY = day;
-
-const typeOfDay = 
-  DAY === 'Saturday' || DAY === 'Sunday'
-  ? 'Weekend'
-  : DAY === 'Wednesday'
-  ? 'Hump Day'
-  : 'Weekday';
-
-console.log(typeOfDay)
+// Logical AND
+let isUserSmiling = true; // false
+isUserSmiling && console.log('User is smiling')
