4d91328cd36d742c6a707c3cc5db19833d239c5f
axai <02b3akshay@gmail.com>
2023-08-05 21:56:39 +0530

    Add Function Expressions-2

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 'use script';
 
+let seconds = 600000;
 
-
-
-const convertTemp = function (temp, convertTo) {
-  let toUnit = convertTo ?? 'F';
-  let tempToConvert = Number(temp) ?? 0;
-
-  if (toUnit === 'F') {
-    let fahrenheit = (tempToConvert * 9) / 5 + 32;
-    return `${fahrenheit}\u{00B0}F`;
-  } else if (toUnit === 'C') { 
-    let celsius = ((tempToConvert - 32) * 5) / 9;
-    return `${celsius}\u{00B0}C`;
-  } else {
-    return `Enter valid units like 'C' or 'F'`
-  }
+const secondsToDays = function (seconds) {
+  return Math.floor(Number(seconds) / (3600 * 24));
 };
 
-let today = 95;
-console.log(convertTemp(today, 'F'));
+console.log(`${seconds} seconds is equal to ${secondsToDays(seconds)} days`);
