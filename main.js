0102a83adf9b559cf9ad0355d339e72287f75147
axai <02b3akshay@gmail.com>
2023-08-06 23:01:53 +0530

    Add Constructor Functions-2

--- main.js
+++ main.js
@@ -1,53 +1,16 @@
 /** @format */
 
-'use strict';
-
-const Product = function (name, description, cost, stock) {
-	this.name = name;
-	this.description = description;
-	this.cost = cost;
-	this.stock = stock;
-	let revenue = 0;
-
-	this.buy = (qty) => {
-		if (qty <= this.stock) {
-			this.stock -= qty;
-			let costOfOrder = qty * this.cost;
-			revenue += costOfOrder;
-			console.log(`${qty} of ${this.name}: $${costOfOrder}`);
-		} else {
-			console.log(`Out of stock!`);
-		}
-	};
-	this.getStock = () =>
-		`Available Quantity (${this.name}): ${this.stock} units\n`;
-
-	this.salesReport = () => `Total sales for ${this.name}: $${revenue}\n`;
+const Person = function (firstname, lastname, age, email) {
+	this.firstName = firstname;
+	this.lastName = lastname;
+	this.age = age;
+	this.email = email;
+	this.introduction = () => `${this.firstName} is ${this.age} years old.`;
+	this.canDrive = () => this.age >= 18 ? 'Yes' : 'No';
 };
 
-const vPhone = new Product(
-	'vPhone 12 pro',
-	'The fastest smartphone ever',
-	800,
-	25,
-);
+const Joe = new Person('Joe', 'Manothan', 19, 'joe@joe.com');
 
-const tapBookPro = new Product(
-	'TapBook Pro V2',
-	'Our thinnest laptop ever',
-	1250,
-	40,
-);
-
-vPhone.buy(2);
-vPhone.buy(12);
-
-
-console.log(vPhone.salesReport());
-console.log(vPhone.getStock());
-
-tapBookPro.buy(2);
-tapBookPro.buy(12);
-tapBookPro.buy(12);
-
-console.log(tapBookPro.salesReport());
+console.log(Joe.introduction());
+console.log(Joe.email);
+console.log("Can Joe Drive : ",Joe.canDrive());
