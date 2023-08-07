b49b9613e9202246bc96bd827dc8fcd87b983b2e
axai <02b3akshay@gmail.com>
2023-08-07 11:23:03 +0530

    Add The for Loop-2

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 /** @format */
 
-let numArray = [
-	{
-		x: 0,
-		y: 1,
-	},
-	{
-		x: 21,
-		y: 13,
-	},
-	{
-		x: 32,
-		y: 89,
-	},
-	{
-		x: 10,
-		y: 11,
-	},
-	{
-		x: 32,
-		y: 78,
-	},
-];
+const fruits = ['Apple', 'Banana', 'Kiwi', 'Strawberry', 'Mango'];
 
-const multNums = (obj) => obj.x + obj.y;
-
-for (let n = 0; n < numArray.length; n++) {
-	console.log(`${numArray[n].x} + ${numArray[n].y} = ${multNums(numArray[n])}`);
+for (let i = 0; i < fruits.length; i++) {
+	if (fruits[i] === 'Kiwi') break;
+	console.log(fruits[i]);
 }
