62fdca425633a9e91fad1cedb9b9ab4b07eb9eb3
axai <02b3akshay@gmail.com>
2023-08-07 12:52:49 +0530

    Add The 'while' and 'do-while' loops-1

--- main.js
+++ main.js
@@ -5,9 +5,22 @@
 let m = 1,
 	oddNums = [];
 
-while (m < 40) {
+while (m < 1) {
 	oddNums.push(m);
 	m += 2;
 }
 
 console.log(oddNums.toString());
+
+// do-while
+
+let n = 1,
+	oddNumsAgain = [];
+
+do {
+	oddNumsAgain.push(n);
+	if (n === 15) break;
+	n += 2;
+} while (n < 1);
+
+console.log(oddNumsAgain);
