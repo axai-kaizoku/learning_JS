c6f43539c5f9758035e1858ea94421cbd850d4df
axai <02b3akshay@gmail.com>
2023-08-07 13:26:47 +0530

    Add Recursive functions-4

--- main.js
+++ main.js
@@ -10,4 +10,6 @@ const calcPower = function (num, power = 1) {
 	return num;
 };
 
-console.log(calcPower(2));
+console.log(calcPower(2, 10));
+console.log(calcPower(10, 2));
+console.log(calcPower(20, 8));
