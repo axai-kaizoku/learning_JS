899c458f06e03e5af463bc44e1ea9d9ad1cbe044
axai <02b3akshay@gmail.com>
2023-08-07 11:55:11 +0530

    Add The 'for-of' and 'for-in' loop-0

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 /** @format */
 
-const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter'];
+// The 'for-of' and 'for-in' loop
+'use strict';
 
-for (let i = 0; i < planets.length; i++) {
-	if (i !== 3) {
-		continue;
-	}
-	console.log(`Perseverance Rover is on ${planets[i]}`);
+const users = [
+	{
+		id: 1,
+		name: 'John',
+		age: 31,
+		profession: 'General Physician',
+	},
+	{
+		id: 2,
+		name: 'jane',
+		age: 29,
+		profession: 'Cardiologist',
+	},
+];
+
+const introduceUser = (user) =>
+	`${user.name} is ${user.age} years old and works as a ${user.profession}`;
+
+for (const user of users) {
+	console.log(introduceUser(user));
 }
