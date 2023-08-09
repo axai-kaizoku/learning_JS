dcd03ee116d57c4dd18cc3eb9e39e6762953bdba
axai <02b3akshay@gmail.com>
2023-08-09 15:33:58 +0530

    Add Creating Arrays using Array.of() and Array.from()-2

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-/** @format */
+'use strict';
 
-const randomNums = (count) =>
-	Array.from(new Array(count), (elem) => parseInt(Math.random() * 100));
+const genStars = (count) => Array.from(Array(count), () => '*')
 
-console.log(randomNums(5));
+for (let i = 1; i <= 6; i++) {
+  console.log(genStars(i));
+}
+
+for (let i = 6; i >= 1; i--) {
+  console.log(genStars(i));
+}
