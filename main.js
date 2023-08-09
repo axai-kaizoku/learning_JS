abcb917a23b4bf823651b8f422874614b6483a2c
axai <02b3akshay@gmail.com>
2023-08-09 09:20:22 +0530

    Add Converting Strings to Arrays and Back-2

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /** @format */
 
-const isPalindrome = (word) => word.split('').reverse().join('') === word;
+const users = `id,first_name,last_name,email,gender
+1,Naruto,Uzumaki,naruto@jp.com,Male
+2,Luffy,D,monkeydluffy@jp.tk.co,Male
+3,Nami,Cat,catbulgluernami@onepiece.com,Female
+4,Ichigo,Kurosaki,ichigok@bleach.co,Male
+5,Light,Yagami,lightyagami@deathnote.jp.com,Male`;
 
-console.log(isPalindrome('civic') ? 'Is a palindrome' : 'Not a palindrome');
+const csvToArray = (csv) => {
+	let output = [];
+	for (const row of csv.split('\n')) {
+		output.push(row.split(','));
+	}
+	return output;
+};
+
+console.table(csvToArray(users));
