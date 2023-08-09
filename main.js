5f13229759a1a479240ae0d689c358828c43225f
axai <02b3akshay@gmail.com>
2023-08-09 09:36:52 +0530

    Add Iteration using forEach()-0

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 /** @format */
 
-'use strict';
+const books = [
+	'The Lord of the Rings',
+	'Deception',
+	'Digital Fortness',
+	'Freakonomics',
+];
 
-const weapons = `1,Red Orbs,240
-2,Green Orbs,194
-3,Yellow Orbs,154
-4,Kill Machine,80`;
-
-const displayWeapons = function (str) {
-	const breakByLine = str.split('\n');
-	for (const item of breakByLine) {
-		let elements = item.split(',');
-		console.log(`${elements[0]}. ${elements[1]} -- $${elements[2]}`);
-	}
-};
-
-displayWeapons(weapons);
-
-// 1. Red Orbs -- $240
-// 2. Green Orbs -- $194
-// 3. Yellow Orbs -- $154
-// 4. Kill Machine -- $80
+books.forEach(function (elem, index) {
+	console.log(elem, index);
+});
