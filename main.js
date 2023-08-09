7b1dfa46a13fe41fafa1e640440ce69fa29b7e65
axai <02b3akshay@gmail.com>
2023-08-09 18:22:15 +0530

    Add Remapping Arrays-1

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-/** @format */
-
-const numArray = [
-	[2, 3],
-	[12, 23],
-	[23, 2],
-	[45, 20.9],
-	[64, 98],
+const users = [
+	{
+		first_name: 'Ichigo',
+		last_name: 'Kurasaki',
+		email: 'kichigo@mappa.com'
+	},
+	{
+		first_name: 'Naruto',
+		last_name: 'Uzumaki',
+		email: 'uzumakinaruto@jp.com'
+	},
+	{
+		first_name: 'Aizen',
+		last_name: 'Kotsuke',
+		email: 'aizenk@bleach.com'
+	},
+	{
+		first_name: 'Luffy',
+		last_name: 'D',
+		email: 'monkeydluffy@op.com'
+	},
 ];
 
-const toSum = (arr) => arr.map(([a, b]) => a + b);
+const genId = () => parseInt(Math.random() * 1000);
+const withIds = users.map((user) => {
+	user.id = genId();
+	return user;
+});
 
-console.log(toSum(numArray));
+console.log(withIds);
