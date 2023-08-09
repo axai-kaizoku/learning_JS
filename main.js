d7e6e8c71eb21908847f2cc7568f85f7dac8e0e5
axai <02b3akshay@gmail.com>
2023-08-09 16:19:23 +0530

    Add Using Spread-2

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 /** @format */
 
-let primaryColor = ['red', 'green', 'blue'];
-let secondaryColor = ['orange', 'green', 'purple'];
+// A todo
 
-let colorWheel = [...primaryColor, ...secondaryColor];
+const Tasks = function (title) {
+	this.title = title ?? 'My tasks';
+	this.taskList = [];
+	this.showTasks = function () {
+		console.log(`----${this.title}----`);
+		this.taskList.forEach((item, index) => console.log(`${index + 1}.${item}`));
+	};
+	this.add = function (item) {
+		this.taskList = [item, ...this.taskList];
+	};
+};
 
-console.log(colorWheel);
+const myTasks = new Tasks('Daily Tasks');
+myTasks.add('Read 10 pages of book');
+myTasks.add('Solve the daily challenge');
+myTasks.showTasks();
+
+const shopping = new Tasks('Shopping');
+shopping.add('Buy HDMI cable');
+shopping.add('Buy new manga chapter');
+shopping.showTasks();
