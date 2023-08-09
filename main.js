a66153454a95cb8640c11c46da249165729c5a9c
axai <02b3akshay@gmail.com>
2023-08-09 09:04:20 +0530

    Add Converting Strings to Arrays and Back-1

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 /** @format */
 
-const str =
-	'House No. 3-4-108, Shanthinagar, Sircilla, Rajanna Sircilla - 505301';
+const isPalindrome = (word) => word.split('').reverse().join('') === word;
 
-// console.log(str.split(', '));
-
-const strng = ['Hello!', 'How are you?', 'Nice to meet you!'];
-
-// for (let i in strng){
-// 	console.log(strng[i])
-// }
-
-console.log(strng.join());
+console.log(isPalindrome('civic') ? 'Is a palindrome' : 'Not a palindrome');
