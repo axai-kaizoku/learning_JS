e0da0dc142c5d876c9d44d13797cc237e1481da7
axai <02b3akshay@gmail.com>
2023-08-09 16:22:53 +0530

    Add Using Spread-3

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 /** @format */
 
-// A todo
+const numArray = [
+	[2, 3],
+	[12, 23],
+	[0, 1],
+	[59, 59.5],
+];
 
-const Tasks = function (title) {
-	this.title = title ?? 'My tasks';
-	this.taskList = [];
-	this.showTasks = function () {
-		console.log(`----${this.title}----`);
-		this.taskList.forEach((item, index) => console.log(`${index + 1}.${item}`));
-	};
-	this.add = function (item) {
-		this.taskList = [item, ...this.taskList];
-	};
-};
+const isGreater = (a, b) =>
+	a > b ? `${a} is greater than ${b}` : `${b} is greater than ${a}`;
 
-const myTasks = new Tasks('Daily Tasks');
-myTasks.add('Read 10 pages of book');
-myTasks.add('Solve the daily challenge');
-myTasks.showTasks();
-
-const shopping = new Tasks('Shopping');
-shopping.add('Buy HDMI cable');
-shopping.add('Buy new manga chapter');
-shopping.showTasks();
+numArray.forEach((elem) => console.log(isGreater(...elem)));
