3472ab3af7af4a600fc8ccc25764344fd2279c5e
axai <02b3akshay@gmail.com>
2023-08-09 12:32:34 +0530

    Add Finding an Element-0

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
-const numbers = [1, 2, 3]
-const squaredNumbers = []
-numbers.forEach(n => squaredNumbers.push(n*n))
-console.log(squaredNumbers)
+/** @format */
+'use strict';
+const mcs = [
+	{
+		id: 1,
+		name: 'luffy',
+		age: 22,
+		anime: 'onepiece',
+	},
+	{
+		id: 2,
+		name: 'naruto',
+		age: 18,
+		anime: 'naruto',
+	},
+	{
+		id: 3,
+		name: 'ichigo',
+		age: 20,
+		anime: 'bleach',
+	},
+	{
+		id: 4,
+		name: 'goku',
+		age: 23,
+		anime: 'dragonball',
+	},
+];
+
+const findMC = (name, arr) =>
+	arr.find((elem) => elem.name === name)?.anime || 'No MC found...';
+
+// console.log(findMC('goku', mcs));
+// console.log(findMC('kira', mcs));
+
+// console.log(mcs.findIndex((elem) => elem.age === 18));
+
+console.log([1, 2, 3, 4].includes(3));
