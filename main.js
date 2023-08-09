5897bfaafbff462e503fbc7f331ce40037031467
axai <02b3akshay@gmail.com>
2023-08-09 11:13:45 +0530

    Add Iteration using forEach-4

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-'use strict';
-
-const books = [
-  {
-    id: 1,
-    title: 'Freakonomics',
-  },
-  {
-    id: 2,
-    title: 'The Design of Everyday Things',
-  },
-  {
-    id: 3,
-    title: 'Shantaram',
-  },
-  {
-    id: 4,
-    title: 'Contact',
-  },
-];
-
-console.log('My Favourite Books');
-
-// Your code here...
-books.forEach((elem, index) => console.log(`${index + 1}. ${elem.title}`));
+const numbers = [1, 2, 3]
+const squaredNumbers = []
+numbers.forEach(n => squaredNumbers.push(n*n))
+console.log(squaredNumbers)
