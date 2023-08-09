065d8edfe9a34f7371ef71874b727f7ad38a1fa3
axai <02b3akshay@gmail.com>
2023-08-09 18:10:05 +0530

    Add Spread Syntax

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-/** @format */
+'use strict';
 
-const numArray = [
-	[2, 3],
-	[12, 23],
-	[0, 1],
-	[59, 59.5],
+const scoreCards = [
+  {
+    id: 1,
+    student: 'Joe M',
+    scores: [54, 76, 65],
+  },
+  {
+    id: 2,
+    student: 'Jane K',
+    scores: [68, 85, 74],
+  },
+  {
+    id: 3,
+    student: 'Chris P',
+    scores: [55, 88, 80],
+  },
 ];
 
-const isGreater = (a, b) =>
-	a > b ? `${a} is greater than ${b}` : `${b} is greater than ${a}`;
+const calculatePercentile = function (english, compSc, mathematics) {
+  return (((english + compSc + mathematics) / 300) * 100).toPrecision(4);
+};
 
-numArray.forEach((elem) => console.log(isGreater(...elem)));
+for (let x of scoreCards) {
+	let scoreArray = x.scores;
+	console.log(`${x.student} has scored ${calculatePercentile(...scoreArray)}%`)
+}
