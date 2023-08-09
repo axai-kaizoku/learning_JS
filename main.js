8a3e16790c74afe67ee3bde4dc5129043766ac29
axai <02b3akshay@gmail.com>
2023-08-09 09:29:39 +0530

    Add Converting Strings to Arrays and Back-3

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 /** @format */
 
-const users = `id,first_name,last_name,email,gender
-1,Naruto,Uzumaki,naruto@jp.com,Male
-2,Luffy,D,monkeydluffy@jp.tk.co,Male
-3,Nami,Cat,catbulgluernami@onepiece.com,Female
-4,Ichigo,Kurosaki,ichigok@bleach.co,Male
-5,Light,Yagami,lightyagami@deathnote.jp.com,Male`;
+'use strict';
 
-const csvToArray = (csv) => {
-	let output = [];
-	for (const row of csv.split('\n')) {
-		output.push(row.split(','));
+const weapons = `1,Red Orbs,240
+2,Green Orbs,194
+3,Yellow Orbs,154
+4,Kill Machine,80`;
+
+const displayWeapons = function (str) {
+	const breakByLine = str.split('\n');
+	for (const item of breakByLine) {
+		let elements = item.split(',');
+		console.log(`${elements[0]}. ${elements[1]} -- $${elements[2]}`);
 	}
-	return output;
 };
 
-console.table(csvToArray(users));
+displayWeapons(weapons);
+
+// 1. Red Orbs -- $240
+// 2. Green Orbs -- $194
+// 3. Yellow Orbs -- $154
+// 4. Kill Machine -- $80
