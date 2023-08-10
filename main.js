36c53ad032d6d72b556bb189d2d3381b51f93529
axai <02b3akshay@gmail.com>
2023-08-10 23:11:08 +0530

    Add Object Destructuring-0

--- main.js
+++ main.js
@@ -2,23 +2,24 @@
 
 'use strict';
 
-const arrC = ['Cheese Cubes', 'Pasta', 'Milk', 'Coke Zero', 'Apples'];
-const arrD = ['Pasta', 'Cheese Cubes', 'Apples', 'Milk', 'Coke Zero'];
-
-const hasSameContents = (arrA, arrB) => {
-	const stringA = arrA.map((el) => JSON.stringify(el));
-	const stringB = arrB.map((el) => JSON.stringify(el));
-	// console.log(stringA)
-
-	return [
-		arrA.length === arrB.length,
-		...stringA.map((el) => stringB.includes(el)),
-		...stringB.map((el) => stringA.includes(el)),
-	].every((el) => el);
+const { title, publisher, authors, cost} = {
+	id: 1,
+	title: 'Freakonomics',
+	authors: [
+		{
+			authorCode: 210,
+			name: 'Steven D. Levitt',
+		},
+		{
+			authorCode: 231,
+			name: 'Stephen J. Dubner',
+		},
+	],
+	publisher: 'Harper Torch',
+	isbn10: '0-06-124270-5',
+	cost: 7.99,
 };
 
 console.log(
-	hasSameContents(arrC, arrD)
-	? 'Both arrays have identical content'
-	: 'Both arrays do not have identical content'
-);
+	`${title} is published by ${publisher} and is written by ${authors.map((el) => el.name).join(' and ')}.It costs $${cost}`
+)
