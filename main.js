e9e3d92866b6f61e116cf1a1fe9f6945650c33db
axai <02b3akshay@gmail.com>
2023-08-10 18:08:05 +0530

    Add Reducing Arrays-1

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
+/** @format */
+
 'use strict';
 
-const numArray = [1, 2, 3, 4, 5]
-const reduced = numArray.reduce((acc, curr) => acc + curr, 0)
-console.log(reduced)
+const cart = [
+	{
+		id: 1,
+		item: 'Beat Headphones',
+		qty: 1,
+		cost: 900,
+	},
+	{
+		id: 2,
+		item: 'HDMI cable',
+		qty: 1,
+		cost: 235.5,
+	},
+	{
+		id: 3,
+		item: 'Dishwasher Detergent',
+		qty: 4,
+		cost: 50,
+	},
+	{
+		id: 4,
+		item: 'Cleanz Rinse Aid',
+		qty: 4,
+		cost: 35,
+	},
+];
+
+const totalCost = cart.reduce((acc, curr) => acc + curr.qty * curr.cost, 0);
+const totalQty = cart.reduce((acc, curr) => acc + curr.qty, 0);
+console.log(`Total Cost: $${totalCost} for ${totalQty} items`);
