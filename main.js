13945208347a34729401b339f0223bbb9165232b
axai <02b3akshay@gmail.com>
2023-08-10 22:59:17 +0530

    Add Comparing Arrays-1

--- main.js
+++ main.js
@@ -2,12 +2,23 @@
 
 'use strict';
 
-const arrA = [1, 2, 3, 1, 23, 10, { id: 1 }, { id: 2 }, 'Hello'];
-const arrB = [1, 2, 3, 1, 23, 10, { id: 1 }, { id: 2 }, 'Hello'];
+const arrC = [1, 2, 3, 1, 23, 10, { id: 1 }, { id: 2 }, 'Hello'];
+const arrD = [1, 2, 3, 1, 23, 10, { id: 1 }, { id: 2 }, 'Hello'];
 
-const isIdentical = (arrA, arrB) => 
-JSON.stringify(arrA) === JSON.stringify(arrB);
+const hasSameContents = (arrA, arrB) => {
+	const stringA = arrA.map((el) => JSON.stringify(el));
+	const stringB = arrB.map((el) => JSON.stringify(el));
+	// console.log(stringA)
+
+	return [
+		arrA.length === arrB.length,
+		...stringA.map((el) => stringB.includes(el)),
+		...stringB.map((el) => stringA.includes(el)),
+	].every((el) => el);
+};
 
 console.log(
-	`Array A ${isIdentical(arrA, arrB) ? 'is' : 'is not'} equal to Array B`
-)
+	hasSameContents(arrC, arrD)
+	? 'Both arrays have identical content'
+	: 'Both arrays do not have identical content'
+);
