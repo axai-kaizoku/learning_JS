6be04a62b1b064a040eaf320a8231c687ae8a4ad
axai <02b3akshay@gmail.com>
2023-08-10 17:22:21 +0530

    Add Filtering Arrays-1

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 'use strict';
 
-function genNum() {
-	const randomNum = [];
-	let num;
-	for (let i = 0; i < 10; i++) {
-		num = Math.floor(10 + Math.random() * 1000);
-		randomNum.push(num);
-	}
-	return randomNum;
-}
+const crew = [
+	{
+		name: 'Luffy',
+		age: 18,
+		role: 'Captain',
+	},
+	{
+		name: 'Zoro',
+		age: 19,
+		role: 'Swordsman',
+	},
+	{
+		name: 'Brook',
+		age: 100,
+		role: 'musician',
+	},
+	{
+		name: 'Nami',
+		age: 21,
+		role: 'navigator',
+	},
+	{
+		name: 'Robin',
+		age: 30,
+		role: 'archeologist',
+	},
+	{
+		name: 'Chopper',
+		age: 16,
+		role: 'doctor',
+	},
+];
 
-const numArray = [...genNum()];
-const evenNums = numArray.filter((elem) => elem % 2 === 0)
-const oddNums = numArray.filter((elem) => elem % 2 !== 0)
+const findCrewmen = ([prop, val], arr) => arr.filter((elem) => elem[prop] === val);
 
-console.log(`\nEven: ${evenNums}\n`)
-console.log(`Odd: ${oddNums}\n`)
-
-console.log(`Original: ${numArray}\n`);
+console.log(findCrewmen(['role', 'navigator'], crew))
