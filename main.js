aae741bbdebaa4f4a529dedb7b48293ffa4d5102
axai <02b3akshay@gmail.com>
2023-08-10 22:50:43 +0530

    Add Comparing Arrays-0

--- main.js
+++ main.js
@@ -1,40 +1,13 @@
+/** @format */
+
 'use strict';
 
-const perfumes = [
-  {
-    id: 1,
-    brand: 'Fruity by Kiwi 50ml',
-    cost: 300,
-    stock: 10,
-  },
-  {
-    id: 2,
-    brand: 'Silly Smelly',
-    cost: 575,
-    stock: 12,
-  },
-  {
-    id: 3,
-    brand: 'Ocean Mist by Ventuo',
-    cost: 1243,
-    stock: 5,
-  },
-  {
-    id: 4,
-    brand: 'Spotlight',
-    cost: 991,
-    stock: 8,
-  },
-  {
-    id: 5,
-    brand: 'Horrendous by Britney',
-    cost: 230,
-    stock: 2,
-  },
-];
+const arrA = [1, 2, 3, 1, 23, 10, { id: 1 }, { id: 2 }, 'Hello'];
+const arrB = [1, 2, 3, 1, 23, 10, { id: 1 }, { id: 2 }, 'Hello'];
 
-const totalInStock = perfumes.reduce((prev, curr) => prev + curr.stock, 0);
-const totalWorth = perfumes.reduce((prev, curr) => prev + curr.cost * curr.stock, 0);
+const isIdentical = (arrA, arrB) => 
+JSON.stringify(arrA) === JSON.stringify(arrB);
 
-console.log(`Total items in stock: ${totalInStock}`);
-console.log(`Total stock worth: $${totalWorth}`);
+console.log(
+	`Array A ${isIdentical(arrA, arrB) ? 'is' : 'is not'} equal to Array B`
+)
