4295909075d0580192bccb8de8dd748e0e275af8
axai <02b3akshay@gmail.com>
2023-08-10 22:46:02 +0530

    Add Reducing Arrays-3

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
-/** @format */
-
 'use strict';
 
-const myArr = [
-	1,
-	2,
-	{
-		id: 1,
-		key: 12,
-	},
-	2,
-	{
-		id: 1,
-		key: 12,
-	},
-	4,
-	5,
+const perfumes = [
+  {
+    id: 1,
+    brand: 'Fruity by Kiwi 50ml',
+    cost: 300,
+    stock: 10,
+  },
+  {
+    id: 2,
+    brand: 'Silly Smelly',
+    cost: 575,
+    stock: 12,
+  },
+  {
+    id: 3,
+    brand: 'Ocean Mist by Ventuo',
+    cost: 1243,
+    stock: 5,
+  },
+  {
+    id: 4,
+    brand: 'Spotlight',
+    cost: 991,
+    stock: 8,
+  },
+  {
+    id: 5,
+    brand: 'Horrendous by Britney',
+    cost: 230,
+    stock: 2,
+  },
 ];
 
-const removeDup = (arr) =>
-	arr.reduce(
-		(acc, curr) =>
-			!acc.some((elem) => JSON.stringify(elem) === JSON.stringify(curr))
-				? [...acc, curr]
-				: acc,
-		[],
-	);
+const totalInStock = perfumes.reduce((prev, curr) => prev + curr.stock, 0);
+const totalWorth = perfumes.reduce((prev, curr) => prev + curr.cost * curr.stock, 0);
 
-console.log(myArr);
-console.log(removeDup(myArr));
+console.log(`Total items in stock: ${totalInStock}`);
+console.log(`Total stock worth: $${totalWorth}`);
