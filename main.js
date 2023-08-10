6679340e87a8b492d6fedc10f7222589b0ba9494
axai <02b3akshay@gmail.com>
2023-08-10 16:39:16 +0530

    Add Using Splice and Slice-1

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
+/** @format */
+
 'use strict';
 
-const keys = ['XDC-102-2206', 'HXC-754-1510', 'XXX-998-0311']
-const dateStrings = keys.map((elem) => elem.slice(8));
-console.log(dateStrings)
-
-const keyCodes = keys.map((elem) => elem.slice(4, 7))
-console.log(keyCodes)
-
-const key = keys.map((elem) => elem.slice(0, 3))
-console.log(key)
-
+const desk = ['Laptop', 'Mouse', 'Speakers', 'Monitors', 'Microphone'];
+desk.splice(1, 1, 'Keyboard', 'Router');
+console.log(desk);
