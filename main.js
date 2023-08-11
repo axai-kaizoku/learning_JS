a476be6e02d69c2b2d29699a3145291e6b866735
axai <02b3akshay@gmail.com>
2023-08-11 16:46:23 +0530

    Add Replacing Text-3 (match)

--- main.js
+++ main.js
@@ -2,15 +2,13 @@
 
 'use strict';
 
-const article =
-	'The movie starring Johnny Depp was released to the masses on a social networking site which created quite a buzz in the industry due to its unconventional approach.';
-	
+const str =
+	'If you have any information on the whereabouts of our cat, please email us at findKibbles@gmail.com and we would get in touch with you right away. You can also mail us at john@mockery.com. Thanks.';
 
-	const findActor = (str, actor) => {
-		const pattern = new RegExp(actor, 'gi');
-		// return pattern.test(str);
-		return str.search(pattern);
-	}
+	const getEmails = (str) => {
+		const pattern = /[a-z0-9._]+@[a-z0-9-]+\.[a-z.]{2,6}/gi;
+		const detect = str.match(pattern);
+		return detect && detect.length > 1 ? detect : detect[0];
+	};
 
-	// console.log(findActor(article, 'johnny deep')) // this doesn't work
-	console.log(findActor(article, 'Johnny Depp'))
+	console.log(getEmails(str))
