aed6564b781dcd043b37366bfd9591ef3606ba51
axai <02b3akshay@gmail.com>
2023-08-11 16:14:50 +0530

    Add Repeating a String-0

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-/** @format */
-
 'use strict';
 
-const quotesFromContact = [
-	'Had he merely announced his plans to the world, certain complications would ensue. If Hadden was frozen to four degrees Kelvin at ten billion kilometers from Earth, what exactly was his legal status?',
-	'Gilgamesh was not equipped with a radio. He no longer wished to know what had happened to the five.',
-];
+const repeatStr = (str, count) =>
+Array.from(Array(count), (elem) => str).join('');
 
-const findString = (str, arr) => {
-	let result = arr.filter((elem) =>
-		elem.toLowerCase().includes(str.toLowerCase()),
-	);
 
-	return result && result.length > 1 ? result : result[0];
-};
-
-console.log(`FINDING "HADDEN": "${findString('hadden', quotesFromContact)}"`);
-
-console.log(
-	`FINDING "GILGAMESH": "${findString('gilgamesh', quotesFromContact)}"`,
-);
+// console.log(repeatStr(' * ', 8))
+console.log('*'.repeat(10))
