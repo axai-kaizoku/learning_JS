656bdad7a4568ae721308d2a6d99b4f6cb1b6073
axai <02b3akshay@gmail.com>
2023-08-11 09:28:32 +0530

    Creating Custom Properties-1

--- main.js
+++ main.js
@@ -2,47 +2,17 @@
 
 'use strict';
 
-const tsla = {
-	stIdx: 2013234,
-	stName: 'TSLA',
-	stCurrPrc: 836.16,
-	stExc: 'NASDAQ',
-	stDetails: {
-		name: 'TESLA, Inc',
-		ceo: 'Elon Musk',
-		hq: 'Paolo Alto, California, United States',
-	},
+const User = function () {
+	Object.defineProperties(this, {
+		id: {
+			enumerable: true,
+			writable: false,
+			configurable: false,
+			value: `UID-${parseInt(Math.random() * 30000)}-XZ`,
+		},
+	});
 };
 
-Object.defineProperty(tsla, 'stIdx', {
-	stIdx : 98876,
-	writable: false,
-	enumerable: false,
-	configurable: false,
-});
-
-Object.defineProperty(tsla, 'stExc', {
-	value: 'NASDA',
-	writable: true,
-	enumerable: false,
-	configurable: false,
-});
-
-for (let i in tsla) {
-	console.log(i)
-}
-
-delete tsla.stExc;
-
-let {
-	stIdx: id,
-	stName: stock,
-	stCurrPrc: value,
-	stExc: exchange,
-	stDetails: { name: company, ceo, hq },
-} = tsla;
-
-
-console.log(`ID: ${id}`);
-console.log(`${stock} is trading for $${value}`);
-console.log(`${company} is headquartered in ${hq} and headed by ${ceo}`);
+let joe = new User();
+// joe.id = '12341297';
+console.log(joe);
