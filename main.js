80de24c87c0e411510dfa2d1a6f63057a330d1ed
axai <02b3akshay@gmail.com>
2023-08-11 21:45:37 +0530

    Add Using startsWith() and endsWith()

--- main.js
+++ main.js
@@ -2,9 +2,29 @@
 
 'use strict';
 
-const message =
-	'       Hello everyone, this is a message from the moon. At approx 9.27am, received a distress call from station X-23       ';
+const data = [
+	'SENSE01,1123,23,55,0.52',
+	'SENSE01,1200,22.3,65,0.77',
+	'SENSE01,1343,23.6,75,0.872',
+	'SENSE02,1234,29.2,85,0.76',
+	'SENSE02,1203,17,20,0.77',
+	'SENSE02,1453,14,45,0.59',
+];
 
-const trimmed = message.trim();
-// console.log(message);
-console.log(trimmed);
+const readDataToObj = (key, arr) =>
+	arr
+		.filter((el) => el.startsWith(key))
+		.filter((el) => el.split(',')[4] > 0.75)
+		.map((el) => {
+			const [sensor, time, temp, humidity, accuracy] = el.split(',');
+
+			return {
+				sensor,
+				time,
+				temp,
+				humidity,
+				accuracy,
+			};
+		});
+
+console.log(readDataToObj('SENSE01', data))
