885a547a477120e3756b34ba9c9a5a326f6ba5b0
axai <02b3akshay@gmail.com>
2023-08-11 16:36:04 +0530

    Add Replacing Text-1 (exec)

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 'use strict';
 const text =
 	'Whether its color or colour, it is all the same. The human eye can see a wide gamut of colours, which makes us unique as a species.';
-const pattern = /colou?rs?/g;
+	
+// const pattern = /colou?rs?/g;
+const pattern = new RegExp('colou?rs?', 'g');
 
 let results;
 while ((results = pattern.exec(text)) !== null) {
