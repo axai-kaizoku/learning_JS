bb87a6f0583d193c3bd97df569c5f6b6ed08c939
axai <02b3akshay@gmail.com>
2023-08-11 11:34:10 +0530

    Add Getting Properties and Values-0

--- main.js
+++ main.js
@@ -1,33 +1,14 @@
 /** @format */
-
 'use strict';
-
-const rectangles = [
-	{
-		id: 'Rectangle - 01',
-		width: 129.2,
-		height: 80.9,
-	},
-	{
-		id: 'Rectangle - 02',
-		width: 342,
-		height: 120,
-		area() {
-			return this.width * this.height;
-		},
-	},
-];
-
-// Your Code Goes Here
-const calcArea = function () {
-	return this.width * this.height;
+const user = {
+	id: 1,
+	name: 'Axai Y',
+	age: 21,
+	address: '3-4-108, ward-32, shanthinagar',
+	city: 'Sircilla',
+	country: 'India',
 };
 
-try {
-	rectangles.forEach((rect) => 
-	console.log(
-		`${rect.id} : ${
-			rect.hasOwnProperty('area') ? rect.area() : calcArea.call(rect)}`));
-} catch (error) {
-	console.log(error.message);
+if (Object.keys(user).includes('country')) {
+	console.log(user.country);
 }
