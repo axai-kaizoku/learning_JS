b938ca4e1a658fb461ddf71c7a64c61aef88d2a1
axai <02b3akshay@gmail.com>
2023-08-11 16:23:53 +0530

    Add Repeating a String-2

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
 /** @format */
 
 'use strict';
-const emotion = (score) => {
-	const emojis = [
-		'\u{1F620}',
-		'\u{1F641}',
-		'\u{1F610}',
-		'\u{1F642}',
-		'\u{1F60d}',
-	];
-	switch (score) {
-		case 1:
-			return emojis[0];
-		case 2:
-			return emojis[1].repeat(2);
-		case 3:
-			return emojis[2].repeat(3);
-		case 4:
-			return emojis[3].repeat(4);
-		case 5:
-			return emojis[4].repeat(5);
-		default:
-			return emojis[2];
-	}
-};
 
-console.log(emotion(5));
+const steps = 6;
+
+for (let x = 1; x <= steps; x++) {
+	console.log(`${' '.repeat(x)}${'*'.repeat(steps + 1 - x)}`);
+}
+
+for (let x = steps; x >= 1; x--) {
+	console.log(`${' '.repeat(x)}${'*'.repeat(steps + 1 - x)}`);
+}
