311744b7fd0ccc27a302bf824070ef8330a6f907
axai <02b3akshay@gmail.com>
2023-08-11 21:16:59 +0530

    Add Replacing text-9

--- main.js
+++ main.js
@@ -2,14 +2,25 @@
 
 'use strict';
 
-const str1 =
-	'The captain of this ship, a dangerous man - Captain Roger was known to have killed 10 whales!!';
+const words = ['Civic:', 'Seltos', 'Car', 'Noon', 'Refer', 'Tenet', 'Design'];
 
-const str2 =
-	'The captain of this ship - a dangerous man, Captain Roger was known to have killed 10 whales!!';
+const isPalindrome = (str) => {
+	const starAProc = str
+		.toLowerCase()
+		.replaceAll(/\s/g, '')
+		.replaceAll(/[:;'"!@#$%^&*]/g, '');
 
-const compareStrings = (str1, str2) =>
-	str1.toLowerCase().replaceAll(/[\W+_-]/gi, '') ===
-	str2.toLowerCase().replaceAll(/[\W+_-]/gi, '');
+	const starBProc = str
+		.toLowerCase()
+		.replaceAll(/\s/g, '')
+		.replaceAll(/[:;'"!@#$%^&*]/g, '')
+		.split('')
+		.reverse()
+		.join('');
 
-console.log(compareStrings(str1, str2));
+	return starAProc === starBProc;
+};
+
+words.forEach((w) => {
+	console.log(`${w} ${isPalindrome(w) ? 'is' : 'is not'} a palindrome`);
+});
