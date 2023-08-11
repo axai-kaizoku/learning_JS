d69afc80021d28d4bc735bf7910f6a57ebd4f2b4
axai <02b3akshay@gmail.com>
2023-08-11 21:50:31 +0530

    Add Using startsWith() and endsWith()-1

--- main.js
+++ main.js
@@ -3,14 +3,27 @@
 'use strict';
 
 const data = [
-	'SENSE01,1123,23,55,0.52',
-	'SENSE01,1200,22.3,65,0.77',
-	'SENSE01,1343,23.6,75,0.872',
-	'SENSE02,1234,29.2,85,0.76',
-	'SENSE02,1203,17,20,0.77',
-	'SENSE02,1453,14,45,0.59',
+	'SENSE_A,1020,12,25,0.52',
+	'SENSE_A,1120,13,26,0.63',
+	'SENSE_A,1222,16,28.5,0.65',
+	'SENSE_A,1330,18,30,0.65',
+	'SENSE_B,1010,10,20,0.65',
+	'SENSE_B,1110,12,21,0.62',
 ];
 
-const byAccuracy = (acc, arr) => arr.filter((el) => el.endsWith(acc));
+const readDataToObj = (key, accuracy, arr) =>
+	arr
+		.filter((el) => el.startsWith(key))
+		.filter((el) => el.endsWith(accuracy))
+		.map((el) => {
+			const [sensor, time, temp, humidity, accuracy] = el.split(',');
 
-console.log(byAccuracy(0.77, data));
+			return {
+				sensor,
+				time,
+				temp,
+				humidity,
+				accuracy,
+			};
+		});
+console.log(readDataToObj('SENSE_A', 0.65, data));
