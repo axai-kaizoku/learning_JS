91bb95a62ac7596a4ccf2216e2b9d8b5c7730948
axai <02b3akshay@gmail.com>
2023-08-12 08:52:29 +0530

    Add Using Date and Time-3

--- main.js
+++ main.js
@@ -2,11 +2,15 @@
 
 'use strict';
 
-const vDay = new Date(2023, 7, 15, 9, 30, 0);
-console.log(`Independence Day: ${vDay.toLocaleString()}`);
+const waltDisneyBirthday = new Date('5 December, 1901 00:35:00');
 
-const whDay = new Date('7 Apr 2023')
-console.log(`World Health Day: ${whDay.toLocaleString()}`)
-console.log(`World Health Day: ${whDay.toDateString()}`)
+const yearsTo = (date) => {
+	const diff = (date = Date.now()) / 3156000000;
+	return Math.abs(diff.toPrecision(4));
+};
 
-// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+console.log(
+	`Walt Disney was born on ${waltDisneyBirthday.toLocaleString(
+		'en-IN',
+	)}. That was ${yearsTo(waltDisneyBirthday)} years ago!`,
+);
