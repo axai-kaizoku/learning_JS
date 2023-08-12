bf45d36411473fa5b62a6818cc4132eba8891ad9
axai <02b3akshay@gmail.com>
2023-08-12 08:40:29 +0530

    Add Using Date and Time-1

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+/** @format */
+
 'use strict';
 
 const today = new Date();
 
-console.log(today.toDateString())
-console.log(today.toISOString())
-console.log(today.toLocaleString())
+console.log(
+	`New Delhi: ${today.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })}`,
+);
+console.log(
+	`Singapore: ${today.toLocaleString('en-US', { timeZone: 'Asia/Singapore' })}`,
+);
+console.log(
+	`New York: ${today.toLocaleString('en-US', {
+		timeZone: 'America/New_York',
+	})}`,
+);
+console.log(`Tokyo: ${today.toLocaleString('en-US', { timeZone: 'Japan' })}`);
