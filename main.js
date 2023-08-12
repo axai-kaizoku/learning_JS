05df43e41e2ba438393b57a96e0077d32b832cec
axai <02b3akshay@gmail.com>
2023-08-12 21:51:04 +0530

    Add Formatting Date and Time-2

--- main.js
+++ main.js
@@ -1,47 +1,25 @@
 'use strict';
 
-// DD, d-MM-YYYY at hh:mm:ss
-const formatDate = (
-	dateObj = new Date(),
-	str = 'DD, d-MM-YYYY at hh:mm:ss',
-) => {
-	const days = [
-		'Sunday',
-		'Monday',
-		'Tuesday',
-		'Wednesday',
-		'Thursday',
-		'Friday',
-		'Saturday',
-	];
+const date = new Date(2021, 6, 21, 22, 10, 0);
 
-	const months = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December',
-	];
-
+const formatDate = (dateObj, str) => {
+	const [day, month, date, year] = dateObj.toDateString().split(/\s/);
+	const [hour, minutes, ...rest] = dateObj.toTimeString().split(':');
+	const [seconds] = rest[0].split(/\s/g);
 	const formattedDate = str
-		.replaceAll('d', dateObj.getDate().toString().padStart(2, 0))
-		.replaceAll('DD', days[dateObj.getDay()])
-		.replaceAll('MM', months[dateObj.getMonth()])
-		.replaceAll('YYYY', [dateObj.getFullYear()])
-		.replaceAll('hh', dateObj.getHours().toString().padStart(2, 0))
-		.replaceAll('mm', dateObj.getMinutes().toString().padStart(2, 0))
-		.replaceAll('ss', dateObj.getSeconds().toString().padStart(2, 0));
-
+		.replaceAll('d', date)
+		.replaceAll('DD', day)
+		.replaceAll('MM', month)
+		.replaceAll('YYYY', year)
+		.replaceAll('hh', hour)
+		.replaceAll('mm', minutes)
+		.replaceAll('ss', seconds);
 	return formattedDate;
 };
 
 console.log(
-	formatDate(new Date('7 April, 2022 22:45'), 'd MM (DD). The time was hh:mm'),
+	`21st june, 2021 can be formatted as ${formatDate(
+		date,
+		'DD, d-MM-YYYY at hh:mm:ss',
+	)}`,
 );
