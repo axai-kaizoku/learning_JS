670a7fbf3f01215693d91905f127d7c7663354a2
axai <02b3akshay@gmail.com>
2023-08-12 10:20:58 +0530

    Add Getter and Setter Methods-1

--- main.js
+++ main.js
@@ -5,19 +5,10 @@
 const log = (str) => console.log(str);
 const now = new Date();
 
-now.setDate(10);
-now.setMonth(3);
-now.setMinutes(30);
-now.setHours(12);
+const showTime = (date = Date()) => {
+	const dateObj = new Date(date);
+	return `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`;
+};
 
-log(now.getDate());
-log(now.getDay());
-log(now.getMonth());
-log(now.getFullYear());
-
-log(now.getHours())
-log(now.getMinutes())
-log(now.getSeconds())
-log(now.getMilliseconds())
-log(now.getTimezoneOffset() / 60)
-log(now.toTimeString())
+log(showTime(new Date('3 April, 2021 20:35')));
+// log(now.toLocaleString())
