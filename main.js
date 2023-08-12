8ebc96a28b9b4087b689c997ec54b49c58be3a96
axai <02b3akshay@gmail.com>
2023-08-12 10:24:47 +0530

    Add Getter and Setter Methods-2

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-/** @format */
-
 'use strict';
 
-const log = (str) => console.log(str);
-const now = new Date();
+const date = new Date(2023, 2, 20);
 
-const showTime = (date = Date()) => {
-	const dateObj = new Date(date);
-	return `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`;
-};
+date.setHours(15)
+date.setMinutes(35)
+date.setSeconds(23)
+date.setMilliseconds(12)
 
-log(showTime(new Date('3 April, 2021 20:35')));
-// log(now.toLocaleString())
+const getDate = date.getDate();
+const getDay = date.getDay();
+const getMonth = date.getMonth();
+const getFullYear = date.getFullYear();
+
+const getHours = date.getHours();
+const getMinutes = date.getMinutes();
+const getSeconds = date.getSeconds();
+const getMilliseconds = date.getMilliseconds();
+const getTime = date.getTime();
+
+console.log(`Date: ${date}`);
+console.log(`The date: ${getDate}`);
+console.log(`The day: ${getDay}`);
+console.log(`The Month: ${getMonth}`);
+console.log(`The year: ${getFullYear}`);
+console.log(`Time: ${getHours}:${getMinutes}:${getSeconds}:${getMilliseconds}`);
+console.log(`Time (ms since Epoch): ${getTime}`);
