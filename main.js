a9ab91fcb8644e6424a55ae0896aa981fe32c8a4
axai <02b3akshay@gmail.com>
2023-08-13 13:28:50 +0530

    Add Integrating JS in a Web Page

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 'use strict';
 
-const date = new Date(2023, 7, 13);
+const createPost = function ({ title, content }) {
+	const socialCard = document.createElement('div');
+	socialCard.setAttribute('class', 'social-card');
+	const icon = document.createElement('div');
+	icon.setAttribute('class', 'post-icon');
+	const postTitle = document.createElement('div');
+	postTitle.setAttribute('class', 'post-title');
+	const postUser = document.createElement('div', 'a');
+	postUser.setAttribute('class', 'post-user');
+	const postContent = document.createElement('div');
+	postContent.setAttribute('class', 'post-content');
 
-const addDays = (dateObj, days = 0) => {
-	const daysToMs = days * 864_00_000;
-	return new Date(dateObj.getTime() + daysToMs);
+	socialCard.append(icon, postTitle, postContent, postUser);
+
+	postTitle.innerText = title;
+	postUser.innerText = 'Joe Mockery';
+	postContent.innerText = content;
+	document.body.prepend(socialCard);
 };
 
-console.log(`Vinayaka Chavithi is on ${addDays(date, 37)}`);
+createPost({
+	title: 'Crazy Summer',
+	content: `This week was crazy with temperatures shooting up all the way to 47 degrees Celsius. At this rate, we'll be BBQed the moment we step out of our houses...`,
+});
+
+createPost({
+	title: 'Remember, Tell me Why??',
+	content: `Back in the 80s, there were a series of books for kids known as the 'Tell Me Why' series from Hamlyn. These were the first few books that I ever got as a child. Pretty amazing. I still have some of em lying around.`,
+});
