50f07ce2e2a6c96e39b18e1e9eb5796b721bf657
axai <02b3akshay@gmail.com>
2023-08-13 11:22:31 +0530

    Add Adding Days to the Date-1

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 'use strict';
-const now = new Date();
-console.log(`Now: ${now.toLocaleString()}`);
 
-const addDays = (dateObj = new Date(), days = 0) => {
+const date = new Date(2023, 7, 13);
+
+const addDays = (dateObj, days = 0) => {
 	const daysToMs = days * 864_00_000;
 	return new Date(dateObj.getTime() + daysToMs);
 };
-console.log(addDays(now, 7).toLocaleString());
 
-const addHours = (dateObj = new Date(), hours = 0) => {
-	const hoursToMs = hours * 36_00_000;
-	return new Date(dateObj.getTime() + hoursToMs);
-};
-console.log(addDays(now, 2).toLocaleString());
-
-const addMinutes = (dateObj = new Date(), minutes = 0) => {
-	const minutessToMs = days * 864_00_000;
-	return new Date(dateObj.getTime() + minutessToMs);
-};
-console.log(addDays(now, 25).toLocaleString());
+console.log(`Vinayaka Chavithi is on ${addDays(date, 37)}`);
