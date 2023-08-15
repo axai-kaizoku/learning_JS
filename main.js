7fd228741123cf2ba1322b0ef9087a5a294e7b5a
axai <02b3akshay@gmail.com>
2023-08-15 16:59:50 +0530

    Add Understanding Events-1

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-const morningBtn = document.querySelector('#morning');
-const dayBtn = document.querySelector('#day');
-const afternoonBtn = document.querySelector('#afternoon');
-const eveningBtn = document.querySelector('#evening');
-const nightBtn = document.querySelector('#night');
-const greeting = document.querySelector('#greeting');
+const quotes = {
+	nelsonMandela: `"The greatest glory in living lies not in never falling, but in rising every time we fall" - Nelson Mandela`,
+	steveJobs: `"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking." - Steve Jobs`,
+	johnLennon: `"Life is what happens when you're busy making other plans." - John Lennon`,
+	anneFrank: `"Whoever is happy will make others happy too." - Anne Frank`,
+};
 
-morningBtn.addEventListener(
+const quoteOut = document.querySelector('#quote');
+const nelsonMBtn = document.querySelector('#nelson-mandela');
+const steveJBtn = document.querySelector('#steve-jobs');
+const johnLBtn = document.querySelector('#john-lennon');
+const anneFBtn = document.querySelector('#anne-frank');
+
+nelsonMBtn.addEventListener(
 	'click',
-	() => (greeting.innerText = 'Good Morning'),
+	() => (quoteOut.innerText = quotes.nelsonMandela),
 );
-
-dayBtn.addEventListener('click', () => (greeting.innerText = 'Good Day'));
-
-afternoonBtn.addEventListener(
+steveJBtn.addEventListener(
 	'click',
-	() => (greeting.innerText = 'Good Afternoon'),
+	() => (quoteOut.innerText = quotes.steveJobs),
 );
-
-eveningBtn.addEventListener(
+johnLBtn.addEventListener(
 	'click',
-	() => (greeting.innerText = 'Good Evening'),
+	() => (quoteOut.innerText = quotes.johnLennon),
 );
-
-nightBtn.addEventListener('click', () => (greeting.innerText = 'Good Night'));
+anneFBtn.addEventListener(
+	'click',
+	() => (quoteOut.innerText = quotes.anneFrank),
+);
