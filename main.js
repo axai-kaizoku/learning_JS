b8134c5d2cf67b58d133cb9eef5838a077a21425
axai <02b3akshay@gmail.com>
2023-08-16 09:05:13 +0530

    Add Playing with Mouse Events

--- main.js
+++ main.js
@@ -1,79 +1,29 @@
-const bug = document.querySelector('.ladybird');
-const upBtn = document.querySelector('#up');
-const downBtn = document.querySelector('#down');
-const rightBtn = document.querySelector('#right');
-const leftBtn = document.querySelector('#left');
-const homeBtn = document.querySelector('#home');
-const turnClockwiseBtn = document.querySelector('#turnClockWise');
-const turnAntiClockwiseBtn = document.querySelector('#turnAntiClockWise');
+const allEmployees = document.querySelector('.all-employees');
+const taskForce = document.querySelector('.task-force');
+const employeeCards = document.querySelectorAll('.employee');
+const { top, left } = allEmployees.getBoundingClientRect();
 
-let rotation = 0;
-let xPos = 0;
-let yPos = 0;
+const createPanel = (x, y, name) => {
+	const createPnl = document.createElement('div');
+	createPnl.setAttribute('class', 'info-panel');
+	createPnl.innerText = name;
+	createPnl.style.top = `${y}px`;
+	createPnl.style.left = `${x}px`;
 
-const turnClockwise = () => {
-	rotation += 90;
-	bug.style.transform = `rotate(${rotation}deg)`;
+	return createPnl;
 };
 
-const turnAntiClockwise = () => {
-	rotation -= 90;
-	bug.style.transform = `rotate(${rotation}deg)`;
-};
+const removePanel = () => document.querySelector('.info-panel')?.remove();
 
-function setTransform() {
-	bug.style.left = `${xPos}px`;
-	bug.style.top = `${yPos}px`;
-}
+allEmployees.addEventListener('contextmenu', function (evt) {
+	evt.preventDefault();
+	removePanel();
+	if (evt.target.getAttribute('class') === 'employee') {
+		const name = evt.target.getAttribute('data-name');
+		const infoPanel = createPanel(evt.clientX - left, evt.clientY - top, name);
 
-const stepUp = () => {
-	yPos > 0 && (yPos += -10);
-	setTransform();
-};
-
-const stepDown = () => {
-	yPos < 450 && (yPos += 10);
-	setTransform();
-};
-
-const stepRight = () => {
-	xPos < 450 && (xPos += 10);
-	setTransform();
-};
-
-const stepLeft = () => {
-	xPos > 0 && (xPos += -10);
-	setTransform();
-};
-
-const home = () => {
-	xPos = 0;
-	yPos = 0;
-	setTransform();
-};
-
-// Mouse Click events
-turnClockwiseBtn.addEventListener('click', turnClockwise);
-turnAntiClockwiseBtn.addEventListener('click', turnAntiClockwise);
-upBtn.addEventListener('click', stepUp);
-downBtn.addEventListener('click', stepDown);
-rightBtn.addEventListener('click', stepRight);
-leftBtn.addEventListener('click', stepLeft);
-homeBtn.addEventListener('click', home);
-
-// Keyboard events
-const keyMapping = {
-	'ArrowUp': stepUp,
-	'ArrowDown': stepDown,
-	'ArrowRight': stepRight,
-	'ArrowLeft': stepLeft,
-	']': turnClockwise,
-	'[': turnAntiClockwise,
-	'H': home,
-	'h': home,
-};
-
-document.addEventListener('keydown', function (event) {
-	const { key } = event;
-	keyMapping[key]();
+		allEmployees.append(infoPanel);
+	}
 });
+
+allEmployees.addEventListener('click', removePanel);
