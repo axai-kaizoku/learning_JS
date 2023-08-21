d2fa303ec2d48dd8e28e2b571f66e2ec9079ac0c
axai <02b3akshay@gmail.com>
2023-08-21 13:10:16 +0530

    Add getters-setters

---
+++ getters-setters/src/js/Product.js
@@ -0,0 +1,24 @@
+class Product {
+	constructor(name, cost, acceptingOrders, quantity, description) {
+		this.name = name;
+		this.cost = cost;
+		this.acceptingOrders = acceptingOrders;
+		this.quantity = quantity;
+		this.description = description;
+	}
+	discount = 0;
+	static tax = 12;
+	set setDiscount(x) {
+		this.discount = x;
+	}
+	get stockCost() {
+		let stockCost = (Product.tax / 100) * this.cost + this.cost * this.quantity;
+		let discAmt = (this.discount / 100) * stockCost;
+		return this.discount === 0 ? stockCost - discAmt : stockCost;
+	}
+	static toUpper(x) {
+		return x.toUpperCase();
+	}
+}
+
+export default Product;
