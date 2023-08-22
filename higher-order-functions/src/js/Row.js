60578319768c814fff65afc0d3890b0b3c947798
axai <02b3akshay@gmail.com>
2023-08-22 16:31:17 +0530

    An update

--- higher-order-functions/src/js/Row.js
+++
@@ -1,6 +0,0 @@
-export default (product, itemCost, quantity, cost) => `<div class="cart-row">
-<div class="item">${product}</div>
-<div class="perItemCost">${itemCost}</div>
-<div class="quantity">${quantity}</div>
-<div class="cost">${cost}</div>
-</div>`;
