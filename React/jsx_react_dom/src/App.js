9c93f63eea9d893100d22ccb279eec5935d0b2e3
axai <02b3akshay@gmail.com>
2023-08-31 19:55:47 +0530

    Add SuperTunes project with REACT

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,62 +1,50 @@
-import React, { Component } from "react";
-import AddProduct from "./components/AddProduct";
-import Products from "./components/Products";
-import getProducts from "./services/getProducts";
+import React, { Component } from 'react';
+import SongCard from './components/SongCard';
+import songs from './services/songs';
 
 class App extends Component {
-  state = {
-    products: [],
-    cart: []
-  };
-  componentDidMount = async () => {
-    const fetchProducts = await getProducts();
-    this.setState({
-      products: fetchProducts
-    });
-  };
-  addProduct = product => {
-    // Find the product in the cart array
-    this.state.cart.find(p => p.id === product.id)
-      ? alert("Product is already in the cart! Please adjust its quantity!")
-      : this.setState({
-          cart: [{ ...product, qty: 1 }, ...this.state.cart]
-        }); // If the product is not in the cart, add it to the top of the array
-  };
-  updateQuantity = (id, qty) => {
-    // Set the quantity of the product
-    qty === 0
-      ? this.setState({
-          cart: this.state.cart.filter(p => p.id !== id)
-        })
-      : this.setState({
-          cart: this.state.cart.map(p => {
-            if (p.id === id) {
-              return { ...p, qty: qty };
-            }
-            return p;
-          })
-        });
-  };
-  getTotals = () => {
-    return this.state.cart.reduce(
-      (prev, curr) => prev + curr.qty * curr.cost,
-      0
-    );
-  };
-  render() {
-    return (
-      <div className="main">
-        <AddProduct data={this.state.products} add={this.addProduct} />
-        <Products
-          data={this.state.cart}
-          update={(id, qty) => this.updateQuantity(id, qty)}
-        />
-        <div className="totals">
-          <div className="totals-v">Total Cost: ${this.getTotals()}</div>
-        </div>
-      </div>
-    );
-  }
+	state = {
+		songs,
+	};
+	sortByTitle = () => {
+		this.setState({
+			songs: [
+				...this.state.songs.sort((a, b) =>
+					a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1,
+				),
+			],
+		});
+	};
+	sortByRating = () => {
+		this.setState({
+			songs: [...this.state.songs.sort((a, b) => b.rating - a.rating)],
+		});
+	};
+	render() {
+		return (
+			<div id="super-tunes">
+				<h2 id="st-title">SuperTunes - Songs of the Week</h2>
+				<button
+					className="st-btn"
+					onClick={this.sortByTitle}>
+					Sort by Title
+				</button>
+				<button
+					className="st-btn"
+					onClick={this.sortByRating}>
+					Sort by Rating
+				</button>
+				<div id="song-list">
+					{this.state.songs.map((song) => (
+						<SongCard
+							key={song.id}
+							data={song}
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
