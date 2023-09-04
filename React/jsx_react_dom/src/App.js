894304b2fcbd391c853a41db6c3a775b3d5eb8df
axai <02b3akshay@gmail.com>
2023-09-04 22:42:54 +0530

    Add ErrorManagement

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,43 +1,42 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import getProducts from "./services/getProducts";
+import Product from "./components/Product";
+import ErrorManager from "./ErrorManager";
 
 const App = () => {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [isComplete, setIsComplete] = useState(false);
-	const registerBtnHandler = () => {
-		if (name && email) {
-			setIsComplete(true);
-		}
-	};
-	return (
-		<div className="app">
-			<h1 id="title">Register</h1>
-			{!isComplete ? (
-				<div className="form-fields">
-					<input
-						id="name-fld"
-						type="text"
-						placeholder="Name"
-						onChange={(e) => setName(e.target.value)}
-					/>
-					<input
-						id="email-fld"
-						type="text"
-						placeholder="E-Mail"
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<button id="register-btn" onClick={registerBtnHandler}>
-						Register
-					</button>
-				</div>
-			) : (
-				<h1 id="confirm">
-					Thank you, {name}! We will contact you shortly on {email}
-				</h1>
-			)}
-		</div>
-	);
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState("");
+  useEffect(() => {
+    getProducts().then(products => setProducts(products));
+  });
+  const filterByCategory = cat => {
+    category === cat ? setCategory("") : setCategory(cat);
+  };
+  return (
+    <div className="container">
+      <ErrorManager onClose={() => setCategory("")}>
+        <div className="categories">
+          {[...new Set(products.map(p => p.type))].map(cat => (
+            <div
+              className={category === cat ? "cat-btn cat-btn-on" : "cat-btn"}
+              key={cat}
+              onClick={() => filterByCategory(cat)}
+            >
+              {cat}
+            </div>
+          ))}
+        </div>
+
+        {category ? (
+          products
+            .filter(p => p.type === category)
+            .map(p => <Product item={p} key={p.id} />)
+        ) : (
+          <p className="pick-text">Pick a category</p>
+        )}
+      </ErrorManager>
+    </div>
+  );
 };
 
 export default App;
