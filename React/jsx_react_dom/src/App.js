f401570a2527acc0cd7f96a795ea73b9071b8c17
axai <02b3akshay@gmail.com>
2023-09-05 12:39:33 +0530

    Add form elements

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,42 +1,12 @@
-import React, {useState, useEffect} from "react";
-import getProducts from "./services/getProducts";
-import Product from "./components/Product";
-import ErrorManager from "./ErrorManager";
+import React from 'react';
+import MessageBox from './MessageBox';
 
-const App = () => {
-  const [products, setProducts] = useState([]);
-  const [category, setCategory] = useState("");
-  useEffect(() => {
-    getProducts().then(products => setProducts(products));
-  });
-  const filterByCategory = cat => {
-    category === cat ? setCategory("") : setCategory(cat);
-  };
-  return (
-    <div className="container">
-      <ErrorManager onClose={() => setCategory("")}>
-        <div className="categories">
-          {[...new Set(products.map(p => p.type))].map(cat => (
-            <div
-              className={category === cat ? "cat-btn cat-btn-on" : "cat-btn"}
-              key={cat}
-              onClick={() => filterByCategory(cat)}
-            >
-              {cat}
-            </div>
-          ))}
-        </div>
-
-        {category ? (
-          products
-            .filter(p => p.type === category)
-            .map(p => <Product item={p} key={p.id} />)
-        ) : (
-          <p className="pick-text">Pick a category</p>
-        )}
-      </ErrorManager>
-    </div>
-  );
-};
+const App = () => (
+	<MessageBox
+		nameVal="Akshay Yelle"
+		natureVal="Tech Support"
+		queryVal="I'm not able to login to my account. I tried resetting my password also. Please fix it ASAP!"
+	/>
+);
 
 export default App;
