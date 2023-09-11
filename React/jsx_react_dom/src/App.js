617af790adc922771ba62693d70ee98c21148c1c
axai <02b3akshay@gmail.com>
2023-09-11 17:26:33 +0530

    Add create own hook-0

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,107 +1,19 @@
-import React, { useState, useReducer } from 'react';
-import TaskContext from './components/TaskContext';
-import Task from './components/Task';
-import { v4 as uuid } from 'uuid';
-
-const taskReducer = (state, action) => {
-	switch (action.type) {
-		case 'ADD_TASK':
-			return {
-				tasks: [{ id: uuid(), ...action.task }, ...state.tasks],
-			};
-		case 'DELETE_TASK':
-			return {
-				tasks: state.tasks.filter((task) => task.id !== action.id),
-			};
-		case 'TOGGLE_DONE':
-			return {
-				tasks: state.tasks.map((task) => {
-					if (task.id === action.id) {
-						return {
-							...task,
-							isDone: !task.isDone,
-						};
-					}
-					return task;
-				}),
-			};
-		case 'SET_PRIORITY':
-			return {
-				tasks: state.tasks.map((task) => {
-					if (task.id === action.id) {
-						return {
-							...task,
-							priority: action.priority,
-						};
-					}
-
-					return task;
-				}),
-			};
-		default: {
-			throw new Error('This action could not be handled!');
-		}
-	}
-};
+import React from 'react';
+import FormPanel from './components/FormPanel';
+import PhoneLogin from './components/PhoneLogin';
+import useResponsive from './hooks/useResponsive';
 
 const App = () => {
-	const [task, setTask] = useState('');
-	const [state, dispatch] = useReducer(taskReducer, { tasks: [] });
+	const { size } = useResponsive();
 	return (
-		<div className="app">
-			<input
-				id="task-input"
-				type="text"
-				placeholder="Add a task..."
-				value={task}
-				onChange={(e) => setTask(e.target.value)}
-				onKeyUp={(e) => {
-					if (e.keyCode === 13) {
-						dispatch({
-							type: 'ADD_TASK',
-							task: {
-								title: task,
-								priority: 'normal',
-								isDone: false,
-							},
-						});
-						setTask('');
-					}
-				}}
-			/>
-
-			<div className="task-list">
-				{state.tasks &&
-					state.tasks.map(({ id, title, priority, isDone }) => (
-						<TaskContext.Provider
-							key={id}
-							value={{
-								id,
-								title,
-								priority,
-								isDone,
-								setPriority: (id, priority) => {
-									dispatch({
-										type: 'SET_PRIORITY',
-										id,
-										priority,
-									});
-								},
-								setIsDone: (id) => {
-									dispatch({
-										type: 'TOGGLE_DONE',
-										id,
-									});
-								},
-								deleteTask: (id) => {
-									dispatch({ type: 'DELETE_TASK', id });
-								},
-							}}
-						>
-							<Task />
-						</TaskContext.Provider>
-					))}
-			</div>
+		<div
+			className={
+				['sm', 'xs'].includes(size) ? 'container vertical' : 'container'
+			}
+		>
+			<div className="cover" />
+			<FormPanel />
+			{['sm ', 'xs'].includes(size) ? <PhoneLogin /> : null}
 		</div>
 	);
 };
--- React/jsx_react_dom/src/components/Priority.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import TaskContext from "./TaskContext";
-
-const Priority = () => {
-  const { priority } = useContext(TaskContext);
-  return <div className={`priority ${priority}`} />;
-};
-
-export default Priority;
