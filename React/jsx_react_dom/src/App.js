7a141af9b941b2188582225901cae11d5cf89521
axai <02b3akshay@gmail.com>
2023-09-11 10:29:22 +0530

    Add useContext hook

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
-import Config from './components/Config';
-import TagsBuilder from './components/TagsBuilder';
+import React, {useState} from "react";
+import ThemePicker from "./components/ThemePicker";
+import Dashboard from "./components/Dashboard";
+import ThemeCtx from "./components/ctx/ThemeCtx";
+import UserCtx from "./components/ctx/UserCtx";
+import StatsCtx from "./components/ctx/StatsCtx";
+
+const userData = {
+  name: "Vincent K",
+  lastLogged: new Date(2019, 11, 5)
+};
+
+const statsData = {
+  aqi: 120,
+  aqText: "Poor air quality. You may face breathing difficulties!"
+};
 
 const App = () => {
-	const [scheduler, setScheduler] = useState(true);
-	const [location, setLocation] = useState(false);
-	const [tags, setTags] = useState([]);
-	return (
-		<div className="container">
-			<Config
-				title="Use Scheduler"
-				toggleState={scheduler}
-				onToggle={(val) => setScheduler(val)}
-			/>
-			<Config
-				title="Use Location"
-				toggleState={location}
-				onToggle={(val) => setLocation(val)}
-			/>
-			<TagsBuilder
-				tags={tags}
-				addTags={(value) =>
-					setTags([
-						...tags,
-						{
-							id: new Date().getTime(),
-							title: value,
-						},
-					])
-				}
-			/>
-		</div>
-	);
+  const [theme, setTheme] = useState({
+    backgroundColor: "rgba(255,255,255)",
+    color: "rgb(0,0,0,0.95)"
+  });
+  return (
+    <div className="container">
+      <ThemePicker onSetTheme={ruleset => setTheme(ruleset)} />
+      <UserCtx.Provider value={userData}>
+        <ThemeCtx.Provider value={{theme}}>
+          <StatsCtx.Provider value={statsData}>
+            <Dashboard />
+          </StatsCtx.Provider>
+        </ThemeCtx.Provider>
+      </UserCtx.Provider>
+    </div>
+  );
 };
 
 export default App;
