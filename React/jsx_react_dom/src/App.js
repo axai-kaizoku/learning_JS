36151efa51ad49d9cce65508d9627fc8142d9129
axai <02b3akshay@gmail.com>
2023-09-19 18:13:56 +0530

    Add codesplitting route

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,31 +1,47 @@
-import React, { useState, lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Spinner from './components/Spinner';
 
 const App = () => {
-	const [showContent, setShowContent] = useState(false);
 	const Photos = lazy(() => import('./components/Photos'));
 	const Posts = lazy(() => import('./components/Posts'));
+
 	return (
-		<div className="App">
-			<div className="header">
-				<button onClick={() => setShowContent(true)} id="show-content">
-					Show Content
-				</button>
-				<button onClick={() => setShowContent(false)} id="hide-content">
-					Hide Content
-				</button>
+		<Router>
+			<div className="App">
+				<div className="header">
+					<Link to="/">
+						<div className="btn">Home</div>
+					</Link>
+					<Link to="/photos" id="photos-btn">
+						<div className="btn">Photos</div>
+					</Link>
+					<Link to="/posts" id="posts-btn">
+						<div className="btn">Posts</div>
+					</Link>
+				</div>
+				<div className="content">
+					<Routes>
+						<Route
+							path="/photos"
+							element={
+								<Suspense fallback={<Spinner />}>
+									<Photos />
+								</Suspense>
+							}
+						/>
+						<Route
+							path="/posts"
+							element={
+								<Suspense fallback={<Spinner />}>
+									<Posts />
+								</Suspense>
+							}
+						/>
+					</Routes>
+				</div>
 			</div>
-			<div className="content">
-				{showContent ? (
-					<>
-						<Suspense fallback={<Spinner />}>
-							<Photos />
-							<Posts />
-						</Suspense>
-					</>
-				) : null}
-			</div>
-		</div>
+		</Router>
 	);
 };
 
