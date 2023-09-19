2c49e07f58ecc8750c3d64a6c9b19e63894e7f0d
axai <02b3akshay@gmail.com>
2023-09-19 17:09:29 +0530

    Add Nested Routes and Parameters

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -13,18 +13,18 @@ const App = () => {
 					<Link to="/" id="home-link">
 						Home
 					</Link>
-					<Link to="/Books" id="books-link">
+					<Link to="/books" id="books-link">
 						Books
 					</Link>
-					<Link to="/Secret" id="secret-link">
+					<Link to="/secret" id="secret-link">
 						Secret Collection
 					</Link>
 				</div>
 				<div className="content">
 					<Routes>
 						<Route exact path="/" element={<Home />} />
-						<Route exact path="/books" element={<Books />} />
-						<Route exact path="/secret" element={<Secret />} />
+						<Route path="/books/:id?" element={<Books />} />
+						<Route path="/secret" element={<Secret />} />
 					</Routes>
 				</div>
 			</div>
