a2f72d71b275b9943cb30984b18c784c8961225f
axai <02b3akshay@gmail.com>
2023-09-19 18:00:03 +0530

    Add codesplitting and suspense-1

--- React/jsx_react_dom/src/App.js
+++ React/jsx_react_dom/src/App.js
@@ -1,32 +1,32 @@
-import React, { useState, lazy, Suspense } from "react";
-import slowLoad from "./slowLoad";
-import Spinner from "./components/Spinner";
-// import Photos from "./components/Photos";
-// import Posts from "./components/Posts";
-const Photos = lazy(() => slowLoad(import("./components/Photos"), 1500));
-const Posts = lazy(() => slowLoad(import("./components/Posts"), 800));
+import React, { useState, lazy, Suspense } from 'react';
+import Spinner from './components/Spinner';
 
 const App = () => {
-  const [showContent, setShowContent] = useState(false);
-
-  return (
-    <div className="App">
-      <div className="header">
-        <button onClick={() => setShowContent(true)}>Show Content</button>
-        <button onClick={() => setShowContent(false)}>Hide Content</button>
-      </div>
-      <div className="content">
-        {showContent ? (
-          <>
-            <Suspense fallback={<Spinner />}>
-              <Photos />
-              <Posts />
-            </Suspense>
-          </>
-        ) : null}
-      </div>
-    </div>
-  );
+	const [showContent, setShowContent] = useState(false);
+	const Photos = lazy(() => import('./components/Photos'));
+	const Posts = lazy(() => import('./components/Posts'));
+	return (
+		<div className="App">
+			<div className="header">
+				<button onClick={() => setShowContent(true)} id="show-content">
+					Show Content
+				</button>
+				<button onClick={() => setShowContent(false)} id="hide-content">
+					Hide Content
+				</button>
+			</div>
+			<div className="content">
+				{showContent ? (
+					<>
+						<Suspense fallback={<Spinner />}>
+							<Photos />
+							<Posts />
+						</Suspense>
+					</>
+				) : null}
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- React/jsx_react_dom/src/slowLoad.js
+++
@@ -1,6 +0,0 @@
-const slowLoad = (module, duration) =>
-  new Promise(resolve => {
-    setTimeout(() => resolve(module), duration);
-  });
-
-export default slowLoad;
