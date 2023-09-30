d33f7507ea9022ffc39b81f3b5f1b02f7674d08d
axai <02b3akshay@gmail.com>
2023-09-30 11:22:21 +0530

    Add find max and min number with randomized Array

--- DataStructures/main.js
+++ DataStructures/main.js
@@ -1,10 +1,24 @@
-const nums = [112, 32, 4, 9, 8, 11];
+const nums = [];
+
+const randomArray = (len) => {
+	for (let i = 0; i < len; i++) {
+		nums.push(Math.floor(Math.random() * 109));
+	}
+};
+
+randomArray(10);
+
+console.log(nums);
+// console.log(nums[0]);
 var maxVal = nums[0];
+var minVal = nums[0];
 
 for (let i = 0; i < nums.length; i++) {
-	if (maxVal > nums[i]) {
+	if (minVal > nums[i]) {
+		minVal = nums[i];
+	} else if (maxVal < nums[i]) {
 		maxVal = nums[i];
 	}
 }
-
 console.log(maxVal);
+console.log(minVal);
