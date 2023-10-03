a1384a7dcd14e4925a31c72b04c3bdee11be93fc
axai <02b3akshay@gmail.com>
2023-10-03 15:28:17 +0530

    Add number of occurances of a value in a list

--- DataStructures/main.js
+++ DataStructures/main.js
@@ -1,77 +1,14 @@
-class HashTable {
-	constructor(size) {
-		this.size = size;
-		this.values = {};
-	}
+const map = new Map();
 
-	calculateHash(value) {
-		return value % this.size;
-	}
-
-	add(value) {
-		var hash = this.calculateHash(value);
-
-		if (this.values[hash] === undefined) {
-			this.values[hash] = value;
-		} else {
-			var iters = 0;
-			while (this.values[hash] !== undefined && iters <= this.size) {
-				hash += 1;
-				hash = hash % this.size;
-				iters += 1;
-			}
-
-			if (iters > this.size) {
-				throw 'StackOverflow';
-			} else {
-				this.values[hash] = value;
-			}
-		}
-	}
-
-	contains(value) {
-		var hash = this.calculateHash(value);
-		if (this.values[hash] === value) {
-			return true;
-		} else {
-			var iters = 0;
-			while (this.values[hash] !== value && iters <= this.size) {
-				hash += 1;
-				hash = hash % this.size;
-				iters += 1;
-			}
-
-			if (this.values[hash] === value) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-
-	print() {
-		console.log(this.values);
-	}
-}
-
-var arr = [1, 2, 3, 3, 4, 4, 2, 4, 5, 6];
-var arr2 = [2, 1, 9, 8, 7, 7, 0];
-
-var h = new HashTable(51);
-var intercept = [];
+const arr = [1, 2, 3, 4, 4, 4, 3, 3, 2, 1, 1, 1, 1, 6, 7, 8, 1];
 
 for (var i = 0; i < arr.length; i++) {
 	var v = arr[i];
-	if (!h.contains(v)) {
-		h.add(v);
+	if (map.has(v)) {
+		map.set(v, map.get(v) + 1);
+	} else {
+		map.set(v, 1);
 	}
 }
 
-for (var i = 0; i < arr2.length; i++) {
-	var v = arr2[i];
-	if (h.contains(v)) {
-		intercept.push(v);
-	}
-}
-
-console.log(intercept);
+console.log(map);
