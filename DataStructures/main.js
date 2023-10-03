dcd2bec9279d6a258cd2628911d2c4eac439a935
axai <02b3akshay@gmail.com>
2023-10-03 15:48:43 +0530

    Add checking if one set is a subset of another

--- DataStructures/main.js
+++ DataStructures/main.js
@@ -1,24 +1,25 @@
 const map = new Map();
 
-var union = [];
 const arr1 = [1, 3, 5, 7, 9];
-const arr2 = [2, 4, 6, 8, 3, 4];
+const arr2 = [9, 7, 5];
 
 for (var i = 0; i < arr1.length; i++) {
 	var v = arr1[i];
 	if (!map.has(v)) {
 		map.set(v, v);
-		union.push(v);
 	}
 }
 
-for (var i = 0; i < arr2.length; i++) {
+var isSubset = true;
+var i = 0;
+
+while (i < arr2.length && isSubset) {
 	var v = arr2[i];
 	if (!map.has(v)) {
-		map.set(v, v);
-		union.push(v);
+		isSubset = false;
 	}
+	i++;
 }
 
-console.log(union);
-// console.log(union.sort());
+console.log(isSubset);
+console.log(map);
