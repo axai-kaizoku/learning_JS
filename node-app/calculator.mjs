33153e84c276a982db12f71d046592f0634ccf09
axai <02b3akshay@gmail.com>
2023-10-07 18:36:21 +0530

    Add module imports and exports demo

---
+++ node-app/calculator.mjs
@@ -0,0 +1,15 @@
+export function add(a, b) {
+	return a + b;
+}
+
+export function subtract(a, b) {
+	return a - b;
+}
+
+export function multiplication(a, b) {
+	return a * b;
+}
+
+export function division(a, b) {
+	return a / b;
+}
---
+++ node-app/index.mjs
@@ -0,0 +1,6 @@
+import * as calculator from './calculator.mjs';
+
+console.log(calculator.add(1, 2));
+console.log(calculator.subtract(1, 2));
+console.log(calculator.multiplication(1, 2));
+console.log(calculator.division(1, 2));
