7c719a0a98c6123d5a7a5c7ff243efed5f85785d
axai <02b3akshay@gmail.com>
2023-10-08 17:40:17 +0530

    Add reading CSV files

--- node-app/index.js
+++ node-app/index.js
@@ -1,13 +1,8 @@
+import * as csv from 'csv-parse';
 import fs from 'fs';
-import { Transform } from 'stream';
 
-const fileReadStream = fs.createReadStream('log.txt');
-const fileWriteStream = fs.createWriteStream('log2.txt');
-
-const trans = new Transform({
-	transform(chunk, encoding, callback) {
-		callback(null, chunk.toString().toUpperCase());
-	},
-});
-
-fileReadStream.pipe(trans).pipe(fileWriteStream);
+fs.createReadStream('log.csv')
+	.pipe(csv.parse({ delimiter: '|' }))
+	.on('data', function (row) {
+		console.log(row);
+	});
