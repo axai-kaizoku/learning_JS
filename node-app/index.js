58ba3499b5be5e5f5356b69d0c628ef6ddcb37d6
axai <02b3akshay@gmail.com>
2023-10-08 22:54:43 +0530

    Add repl initialization

--- node-app/index.js
+++ node-app/index.js
@@ -1,16 +1,3 @@
-import * as csv from 'csv-parse';
-import fs from 'fs';
+import repl from 'repl';
 
-let sum = 0;
-
-fs.createReadStream('transactions.csv')
-	.pipe(csv.parse({ delimiter: ',' }))
-	.on('data', function (row) {
-		sum += parseFloat(row[1]);
-	})
-	.on('end', () => {
-		console.log(sum);
-	})
-	.on('error', (err) => {
-		console.log(err);
-	});
+repl.start();
