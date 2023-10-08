307892ee14284465d5feb19a5e1dc48bfd0799d6
axai <02b3akshay@gmail.com>
2023-10-08 17:52:33 +0530

    Add writing formatted csv files

--- node-app/index.js
+++ node-app/index.js
@@ -1,8 +1,20 @@
-import * as csv from 'csv-parse';
+import * as stringify from 'csv-stringify';
 import fs from 'fs';
 
-fs.createReadStream('log.csv')
-	.pipe(csv.parse({ delimiter: '|' }))
-	.on('data', function (row) {
-		console.log(row);
-	});
+const write = fs.createWriteStream('log2.txt');
+const data = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+	[10, 11, 12],
+];
+
+const columns = ['num1', 'num2', 'num3'];
+
+const stringifier = stringify.stringify({ header: true, columns: columns });
+
+for (let i = 0; i < data.length; i++) {
+	stringifier.write(data[i]);
+}
+
+stringifier.pipe(write);
