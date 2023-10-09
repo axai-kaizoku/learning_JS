324fd05ddb977b84be77354f09f5e954162b908f
axai <02b3akshay@gmail.com>
2023-10-09 11:20:12 +0530

    Add readline interface

--- node-app/index.js
+++ node-app/index.js
@@ -2,8 +2,12 @@ import readline from 'readline';
 
 let r1 = readline.createInterface(process.stdin, process.stdout);
 
-r1.question('What is your name ? \n', (name) => {
-	r1.write(`Your name in UPPERCASE: ${name.toUpperCase()}`);
-	// console.log(`Your name in UPPERCASE: ${name.toUpperCase()}`);
-	r1.close();
+r1.on('SIGINT', () => {
+	console.log('This is a testing create Interface');
+	r1.question('Exit (y or n)', (input) => {
+		console.log('This is a testing create Interface');
+		if (input === 'y') {
+			r1.pause();
+		}
+	});
 });
