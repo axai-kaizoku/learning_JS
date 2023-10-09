315da39decc4c03f31ad672627791305b2feae05
axai <02b3akshay@gmail.com>
2023-10-09 15:35:39 +0530

    Add synchronous events (event module)

--- node-app/index.js
+++ node-app/index.js
@@ -3,9 +3,16 @@ import { EventEmitter } from 'events';
 const myEventEmitter = new EventEmitter();
 
 function notifyConnection(ip) {
-	console.log(ip + ' connected to server!');
+	console.log(ip + ' connected to server! (notifyConnection)');
 }
 
+const process = async (ip) => {
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	console.log(`${ip} connected to server! (process)`);
+};
+
 myEventEmitter.on('connection', notifyConnection);
+myEventEmitter.on('connection', process);
 
 myEventEmitter.emit('connection', '127.0.0.1');
+myEventEmitter.emit('connection', '192.168.0.2');
