f35ffcc5dcdcbfcbf70403783251851ac33d8217
axai <02b3akshay@gmail.com>
2023-10-12 16:44:46 +0530

    Add Hashing a text

--- node-app/crypt_vid.js
+++ node-app/crypt_vid.js
@@ -1,34 +1,7 @@
 import crypto from 'crypto';
 
-const algorithm = 'aes-256-cbc';
+const algorithm = 'sha256';
 const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
 
-function encrypt(text) {
-	let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-	let encrypted = cipher.update(text);
-	encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-	return {
-		iv: iv.toString('hex'),
-		key: key.toString('hex'),
-		encryptedData: encrypted.toString('hex'),
-	};
-}
-
-function decrypt(text) {
-	let iv = Buffer.from(text.iv, 'hex');
-	let encryptedText = Buffer.from(text.encryptedData, 'hex');
-
-	let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-	let decrypted = decipher.update(encryptedText);
-	decrypted = Buffer.concat([decrypted, decipher.final()]);
-
-	return decrypted.toString();
-}
-
-let input = encrypt('Yeah boy');
-console.log('Encrypted text\n');
-console.log(input);
-console.log('Decrypted text\n');
-console.log(decrypt(input));
+const hash = crypto.createHash(algorithm, key).update('test').digest('hex');
+console.log(hash);
