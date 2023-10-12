2faadbdfaed980cc5d8deed04d5d8f5b13152905
axai <02b3akshay@gmail.com>
2023-10-12 15:03:18 +0530

    Add routing data

--- node-app/server.js
+++ node-app/server.js
@@ -1,19 +1,40 @@
 import http2 from 'http2';
 
-const server = http2.createServer();
-
-server.on('error', (err) => {
-	console.log(err);
-});
-
-server.on('stream', (stream, headers) => {
+const helloHandler = (stream, headers) => {
+	console.log(headers);
 	stream.respond({
 		':status': 200,
 	});
-	stream.write('Hello!');
-	stream.write(Date.now().toString());
-	stream.end();
-});
+	stream.write('Getting huh?');
+	stream.end('Hello!');
+};
+
+const notFoundHandler = (stream, headers) => {
+	stream.respond({
+		':status': 404,
+	});
+	stream.write('404 x_x');
+	stream.end('Path not found!');
+};
+
+const server = http2.createServer();
+
+const router = (stream, headers) => {
+	const path = headers[':path'];
+	const method = headers[':method'];
+
+	let handler;
+
+	if (path === '/' && method === 'GET') {
+		handler = helloHandler;
+	} else {
+		handler = notFoundHandler;
+	}
+
+	handler(stream, headers);
+};
+
+server.on('stream', router);
 
 server.listen(8000, () => {
 	console.log('Server is running');
