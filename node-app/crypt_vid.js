4bbe7e6e0d31ee73853ab2f564457c9596cb2957
axai <02b3akshay@gmail.com>
2023-10-12 17:07:42 +0530

    Add Hashing a user:password in http server

--- node-app/crypt_vid.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-const algorithm = 'sha256';
-const key = crypto.randomBytes(32);
-
-const hash = crypto.createHash(algorithm, key).update('test').digest('hex');
-console.log(hash);
---
+++ node-app/httpServer.js
@@ -0,0 +1,59 @@
+import http from 'http';
+import url from 'url';
+import crypto from 'crypto';
+
+let users = [];
+const key = crypto.randomBytes(32);
+
+http
+	.createServer((req, res) => {
+		if (req.method === 'POST') {
+			let body = '';
+
+			req.on('data', (data) => {
+				body += data;
+			});
+
+			req.on('end', () => {
+				body = JSON.parse(body);
+
+				const index = users.indexOf(users.username);
+				if (index === -1) {
+					const hash = crypto.createHash('sha256', key);
+					users[body.username] = hash.update(body.password).digest('hex');
+				}
+				console.log(users);
+			});
+		} else if (req.method === 'PUT') {
+			let body = '';
+			req.on('data', (data) => {
+				body += data;
+			});
+			req.on('end', () => {
+				body = JSON.parse(body);
+				const hash = crypto.createHash('sha256', key);
+				users[body.username] = hash.update(body.password).digest('hex');
+				console.log(users);
+			});
+		} else if (req.method === 'DELETE') {
+			let body = '';
+
+			req.on('data', (data) => {
+				body += data;
+			});
+
+			req.on('end', () => {
+				body = JSON.parse(body);
+				const index = users.indexOf(users.username);
+				if (index > -1) {
+					users.splice(index, 1);
+				}
+				console.log(users);
+			});
+		} else if (req.method === 'GET') {
+			const parse = url.parse(req.url);
+			console.log(users[parse.query]);
+		}
+		res.end();
+	})
+	.listen(80);
