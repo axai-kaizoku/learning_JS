4427e0104b5dc933879ef10222abc82c12781b47
axai <02b3akshay@gmail.com>
2023-10-13 12:28:55 +0530

    Add Interprocess communication (child and parent thread communication)

--- node-app/worker.js
+++ node-app/worker.js
@@ -4,16 +4,14 @@ import http from 'http';
 const numCpus = 4;
 
 if (cluster.isPrimary) {
-	for (let i = 0; i < numCpus; i++) {
-		cluster.fork();
-	}
+	const worker = cluster.fork();
+	worker.send('Hello!');
+	worker.on('message', (msg) => {
+		console.log(`msg: ${msg} from worker #${worker.id}`);
+	});
 } else {
-	http
-		.createServer((req, res) => {
-			res.writeHead(200);
-			res.end(`Process ${process.pid} says hello!`);
-		})
-		.listen(8000, () => {
-			console.log('Server is up and running!\n     ', process.pid);
-		});
+	process.on('message', (msg) => {
+		process.send(msg);
+	});
+	process.send('Message from worker!');
 }
