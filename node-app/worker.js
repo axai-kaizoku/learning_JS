d4992376124db78f0d265531d221c5b3878c6593
axai <02b3akshay@gmail.com>
2023-10-13 12:43:50 +0530

    Add Process management using pm2

--- node-app/worker.js
+++ node-app/worker.js
@@ -7,7 +7,7 @@ if (cluster.isPrimary) {
 	const worker = cluster.fork();
 	worker.send('Hello!');
 	worker.on('message', (msg) => {
-		console.log(`msg: ${msg} from worker #${worker.id}`);
+		console.log(`msg: ${msg} from worker #${worker.id} `);
 	});
 } else {
 	process.on('message', (msg) => {
