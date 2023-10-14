ff20848a6268a17355f6d1498892c6db3e190f70
axai <02b3akshay@gmail.com>
2023-10-14 16:50:54 +0530

    Add setting up templating language pug

--- blogChef-express/app.js
+++ blogChef-express/app.js
@@ -3,17 +3,47 @@ import { join } from 'path';
 const app = express();
 
 app.use('/assets', express.static(join(__dirname, 'public')));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.status(200).send('<h1>Blog Chef says Hello!</h1>');
+	res.send('<h1>Blog Chef </h1>');
 });
 
 app
 	.get('/admin/login', (req, res) => {
-		res.sendFile(join(__dirname, 'views', 'login.html'));
+		res.render('login');
 	})
 	.post('/admin/login', (req, res) => {
-		res.send('Handle Login Here!');
+		res.redirect('/admin/dashboard');
 	});
+
+app.get('/admin/dashboard', (req, res) => {
+	res.render('dashboard', {
+		user: 'Joe Mockery',
+		posts: [
+			{
+				id: 1,
+				author: 'Axai Y',
+				title: 'I love express',
+				content:
+					'The express frame work is more convenient to use and not strict to libraries',
+			},
+			{
+				id: 2,
+				author: 'Carry Minati',
+				title: 'I love PUG',
+				content: "It's a great templating language and very easy to use!",
+			},
+		],
+	});
+});
+
+app.post('/admin/approve', (req, res) => {
+	res.redirect('/admin/dashboard');
+});
+
+app.get('/admin/logout', (req, res) => {
+	res.redirect('/admin/login');
+});
 
 app.listen(3000, () => console.log('Blog Chef is cooking on port 3000'));
