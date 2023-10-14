9a2ddb84a90e39949598930acc9398903ef09b29
axai <02b3akshay@gmail.com>
2023-10-14 14:50:54 +0530

    Add palindrome checking in express response

--- blogChef-express/app.js
+++ blogChef-express/app.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import { parse } from 'querystring';
 const app = express();
+
+const isPalindrome = (str) => {
+	let trimAndPrepare = str.toLowerCase().trim().replace(/[\W_]/g, '');
+	return trimAndPrepare === trimAndPrepare.split('').reverse().join('');
+};
 
 app.get('/', (req, res) => {
 	res.status(200).send('<h1>Blog Chef says Hello!</h1>');
+});
+
+app.post('/palindrome', (req, res) => {
+	let body = '';
+	req.on('data', (data) => (body += data));
+	req.on('end', () => {
+		let { word } = parse(body);
+		res.send(
+			word
+				? { word, isPalindrome: isPalindrome(word) }
+				: { message: 'No word supplied!' },
+		);
+	});
 });
 
 app.listen(3000, () => console.log('Blog Chef is cooking on port 3000'));
