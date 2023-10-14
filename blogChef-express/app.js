50a1cf398b47ba4d6cfcfad78f0f76b2ddcf04a1
axai <02b3akshay@gmail.com>
2023-10-14 15:12:46 +0530

    Add setting up static files using express.static - middleware

--- blogChef-express/app.js
+++ blogChef-express/app.js
@@ -1,27 +1,19 @@
 import express from 'express';
-import { parse } from 'querystring';
+import { join } from 'path';
 const app = express();
 
-const isPalindrome = (str) => {
-	let trimAndPrepare = str.toLowerCase().trim().replace(/[\W_]/g, '');
-	return trimAndPrepare === trimAndPrepare.split('').reverse().join('');
-};
+app.use('/assets', express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.status(200).send('<h1>Blog Chef says Hello!</h1>');
 });
 
-app.post('/palindrome', (req, res) => {
-	let body = '';
-	req.on('data', (data) => (body += data));
-	req.on('end', () => {
-		let { word } = parse(body);
-		res.send(
-			word
-				? { word, isPalindrome: isPalindrome(word) }
-				: { message: 'No word supplied!' },
-		);
+app
+	.get('/admin/login', (req, res) => {
+		res.sendFile(join(__dirname, 'views', 'login.html'));
+	})
+	.post('/admin/login', (req, res) => {
+		res.send('Handle Login Here!');
 	});
-});
 
 app.listen(3000, () => console.log('Blog Chef is cooking on port 3000'));
