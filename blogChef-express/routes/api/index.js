c0f966fa4d838a4eb9d1d6ed19728ae8a8db5ffa
axai <02b3akshay@gmail.com>
2023-10-15 13:43:25 +0530

    Add Dynamic Routing and route params

--- blogChef-express/routes/api/index.js
+++ blogChef-express/routes/api/index.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import getPosts from './get-posts';
 import loginUser from './login-user';
 import signUpUser from './signup-user';
+import getPost from './get-post';
+import storePost from './store-post';
+import deletePost from './delete-post';
+import catchAll from './catch-all';
 
 const router = Router();
 
 router.get('/posts', getPosts);
+router.route('/post/:postId?').get(getPost).post(storePost).delete(deletePost);
 router.post('/login', loginUser);
 router.post('/signup', signUpUser);
+router.use(catchAll);
 
 export default router;
---
+++ blogChef-express/routes/api/store-post.js
@@ -0,0 +1,6 @@
+export default (req, res) => {
+	const post = req.body;
+	res.json({
+		post,
+	});
+};
