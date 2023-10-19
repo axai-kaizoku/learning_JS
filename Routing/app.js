309911650d8395240ff689b6b11ded5eb7c113e9
axai <02b3akshay@gmail.com>
2023-10-19 19:15:41 +0530

    Add sending response and saving in a JSON file locally(activity)

--- Routing/app.js
+++ Routing/app.js
@@ -1,70 +1,38 @@
 import express from 'express';
-import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import fs from 'fs';
 
 const app = express();
+const PORT = 3000;
+const MESSAGES_PATH = './messages.json';
 
-app.set('view engine', 'pug');
-app.use(express.urlencoded({ extended: true }));
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(morgan(':method - :url - :date -:response-time ms'));
+// Route to handle form submission
+app.post('/contact', (req, res) => {
+	const formData = {
+		name: req.body.name,
+		email: req.body.email,
+		message: req.body.message,
+		date: new Date().toISOString(),
+	};
 
-app.get('/', (req, res) => {
-	res.render('home');
+	// Read existing messages from JSON file
+	let messages = [];
+	if (fs.existsSync(MESSAGES_PATH)) {
+		const rawData = fs.readFileSync(MESSAGES_PATH);
+		messages = JSON.parse(rawData);
+	}
+
+	// Add new message to the array and save to JSON file
+	messages.push(formData);
+	fs.writeFileSync(MESSAGES_PATH, JSON.stringify(messages));
+
+	res.send('<h1>Thank you for your message! It has been stored.</h1>');
 });
 
-app.post('/', (req, res) => {
-	res.json({ message: 'Your post request is handled!' });
-});
-
-app.get('/admin', (req, res) => {
-	res.send(
-		'<h1>This is Admin Route</h1>\n<button><a href="/">Home</a></button>',
-	);
-});
-
-// Sample in-memory data storage
-let posts = [];
-
-// Routes
-
-// Get all posts
-app.get('/posts', (req, res) => {
-	res.json(posts);
-});
-
-// Create a new post
-app.post('/posts', (req, res) => {
-	// const post = {
-	// 	id: Date.now(),
-	// 	title: req.body.title,
-	// 	content: req.body.content,
-	// };
-	// posts.push(post);
-	// res.status(201).json(post);
-	const request = req;
-	console.log(request);
-});
-
-// Update a post by ID
-app.put('/posts/:id', (req, res) => {
-	const post = posts.find((p) => p.id === parseInt(req.params.id));
-	if (!post) return res.status(404).send('Post not found');
-
-	post.title = req.body.title || post.title;
-	post.content = req.body.content || post.content;
-
-	res.json(post);
-});
-
-// Delete a post by ID
-app.delete('/posts/:id', (req, res) => {
-	const postIndex = posts.findIndex((p) => p.id === parseInt(req.params.id));
-	if (postIndex === -1) return res.status(404).send('Post not found');
-
-	const deletedPost = posts.splice(postIndex, 1);
-	res.json(deletedPost);
-});
-
-app.listen(3000, () => {
-	console.log('Server is running!');
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
